823656dd187e41a61de0413c11ce7fdc546e168e
freeok <51998152+freeok@users.noreply.github.com>
2024-03-30 18:38:12 +0800

    chore: Update workflow 文件

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -1,25 +1,28 @@
+# 测试通过
+
+rm -rf dist
+
 mvn clean package "-DskipTests"
 
-cd target
-mkdir tmp
-mv app-jar-with-dependencies.jar tmp
-mv classes tmp
-pwd
-ls
-cd ..
-pwd
-ls
+mkdir target/jpackage
+mv target/app-jar-with-dependencies.jar target/jpackage
 
+# 在 linux 环境会转换为 .deb 文件
+# type=app-image 记得去掉--about-url
 jpackage \
---name "So Novel" \
---input "target/tmp" \
+--name "SoNovel" \
+--input "target/jpackage" \
 --dest dist \
 --icon assets/logo.ico \
---app-version 1.5.1 \
+--app-version 1.5.3 \
 --copyright "Copyright (C) 2024 pcdd. All rights reserved." \
 --description "开源搜书神器" \
 --vendor "github.com/pcdd-group" \
 --about-url "github.com/pcdd-group/so-novel" \
---main-jar app-jar-with-dependencies.jar
+--main-jar "app-jar-with-dependencies.jar"
 
-tar -zcvf app.tar.gz dist
+cp config.ini dist/SoNovel
+rm dist/SoNovel/*.ico
+
+echo "开始打包压缩"
+tar czvf dist/SoNovel_linux.tar.gz dist/SoNovel
