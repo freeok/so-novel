31d7eff292be18abaa97f7e3e56a7b80b63ab508
pcdd <akapcdd@163.com>
2024-03-31 17:14:52 +0800

    refactor: 移动 logo.ico

--- bin/jpackage-windows.sh
+++ bin/jpackage-windows.sh
@@ -9,7 +9,7 @@ jpackage --version
 echo "开始构建exe"
 
 # GitHub Actions 日志: "Error: Invalid Option: [target]" 一个奇怪的错误
-jpackage --name "SoNovel" --type "app-image" --input "nobug/jpackage" --dest dist --icon input/logo.ico --app-version 1.5.3 --copyright "Copyright (C) 2024 pcdd. All rights reserved." --description "开源搜书神器" --vendor "github.com/pcdd-group" --main-jar "app-jar-with-dependencies.jar" --win-console
+jpackage --name "SoNovel" --type "app-image" --input "nobug/jpackage" --dest dist --icon assets/logo.ico --app-version 1.5.3 --copyright "Copyright (C) 2024 pcdd. All rights reserved." --description "开源搜书神器" --vendor "github.com/pcdd-group" --main-jar "app-jar-with-dependencies.jar" --win-console
 
 # cp: cannot create regular file 'dist/SoNovel': No such file or directory
 cp config.ini dist/SoNovel
@@ -42,7 +42,7 @@ tar czvf dist/sonovel-win.tar.gz dist/SoNovel
 #--win-console \
 #--input "target/tmp" \
 #--dest dist \
-#--icon input/logo.ico \
+#--icon assets/logo.ico \
 #--app-version 1.5.3 \
 #--copyright "Copyright (C) 2024 pcdd. All rights reserved." \
 #--description "开源搜书神器" \
@@ -55,7 +55,7 @@ tar czvf dist/sonovel-win.tar.gz dist/SoNovel
 #--name "SoNovel" `
 #--input "target/tmp" `
 #--dest dist `
-#--icon input/logo.ico `
+#--icon assets/logo.ico `
 #--app-version 1.5.3 `
 #--copyright "Copyright (C) 2024 pcdd. All rights reserved." `
 #--description "开源搜书神器" `
