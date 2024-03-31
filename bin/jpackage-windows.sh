e3b817aedf7b46785e8d33e76f0a22c915d7da44
freeok <51998152+freeok@users.noreply.github.com>
2024-03-31 14:27:18 +0800

    移动 logo.ico

--- bin/jpackage-windows.sh
+++ bin/jpackage-windows.sh
@@ -9,7 +9,7 @@ jpackage --version
 echo "开始构建exe"
 
 # TODO fix Error: Invalid Option: [target]
-jpackage --name "SoNovel" --type "app-image" --input "nobug/jpackage" --dest dist --icon assets/logo.ico --app-version 1.5.3 --copyright "Copyright (C) 2024 pcdd. All rights reserved." --description "开源搜书神器" --vendor "github.com/freeok" --main-jar "app-jar-with-dependencies.jar" --win-console
+jpackage --name "SoNovel" --type "app-image" --input "nobug/jpackage" --dest dist --icon input/logo.ico --app-version 1.5.3 --copyright "Copyright (C) 2024 pcdd. All rights reserved." --description "开源搜书神器" --vendor "github.com/freeok" --main-jar "app-jar-with-dependencies.jar" --win-console
 
 # cp: cannot create regular file 'dist/SoNovel': No such file or directory
 cp config.ini dist/SoNovel
