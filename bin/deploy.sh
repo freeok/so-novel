78c3855cea5c069b34a3b5ca0cff4d9c28343e71
pcdd <akapcdd@163.com>
2024-03-31 14:50:57 +0800

    chore: 新增一种 Release Assets

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,17 @@
 # launch4j-maven-plugin 仅支持 windows
 
+# 定义 Maven 命令并保存到变量中
+maven_command=""
+artifacts=""
+# 根据传入的参数执行不同的操作
+if [ "$1" == "jre" ]; then
+  maven_command="mvn clean package -DskipTests -DjrePath=runtime"
+  artifacts="sonovel-win-with-jre.tar.gz"
+else
+  maven_command="mvn clean package -DskipTests"
+  artifacts="sonovel-win.tar.gz"
+fi
+
 # 项目根目录，根据当前文件所在路径获取相对路径
 project_path=$(
   cd "$(dirname "$0")" || exit
@@ -8,8 +20,11 @@ project_path=$(
 )
 cd "$project_path" || exit
 
-mvn clean package "-DskipTests"
+$maven_command
 cp config.ini target/SoNovel
+if [ "$1" == "jre" ]; then
+  cp input/*.txt input/*.rar target/SoNovel
+fi
 
 cd target
-tar czvf SoNovel_win.tar.gz SoNovel
+tar czvf $artifacts SoNovel
