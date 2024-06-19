83651632f6d618a105a2eaea23d58de68f6bb989
pcdd <akapcdd@163.com>
2024-06-19 12:42:19 +0800

    refactor: 重构ChapterFilter.java

--- src/main/java/com/pcdd/sonovel/core/CrawlerPostHandler.java
+++ src/main/java/com/pcdd/sonovel/core/CrawlerPostHandler.java
@@ -60,6 +60,7 @@ public class CrawlerPostHandler {
         book.getMetadata().addTitle(b.getBookName());
         book.getMetadata().addAuthor(new Author(b.getAuthor()));
         book.getMetadata().addDescription(b.getDescription());
+        // 不设置会导致部分阅读器出现问题（例如ibooks无法使用中文字体）
         book.getMetadata().setLanguage("zh");
         byte[] bytes = HttpUtil.downloadBytes(b.getCoverUrl());
         book.setCoverImage(new Resource(bytes, ".jpg"));
