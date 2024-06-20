260b5f0b1eafc2b25c363a49e990bf14b755f3e6
freeok <akapcdd@163.com>
2024-06-20 13:54:32 +0800

    feat: add -Dconfig.file

--- src/main/java/com/pcdd/sonovel/util/Settings.java
+++ src/main/java/com/pcdd/sonovel/util/Settings.java
@@ -1,5 +1,7 @@
 package com.pcdd.sonovel.util;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Console;
 import cn.hutool.setting.dialect.Props;
 import lombok.experimental.UtilityClass;
 
@@ -14,27 +16,31 @@ import java.nio.file.Paths;
 @UtilityClass
 public class Settings {
 
+    /**
+     * 从 application.properties 加载系统属性
+     */
     public Props sys() {
         return Props.getProp("application.properties", StandardCharsets.UTF_8);
     }
 
+    /**
+     * 从 config.ini 加载用户属性
+     */
     public Props usr() {
-        try {
-            String configFilePath = System.getProperty("config.file");
+        // 从虚拟机选项 -Dconfig.file 获取用户配置文件路径
+        String configFilePath = System.getProperty("config.file");
 
-            if ((configFilePath == null) || configFilePath.isEmpty()) {
-                throw new IllegalArgumentException(
-                    "Config file path is not specified or is empty.");
-            }
-
-            Path absolutePath = Paths.get(configFilePath).toAbsolutePath();
-
-            return Props.getProp(absolutePath.toString(), StandardCharsets.UTF_8);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-
-            return null;
+        // 若未指定或指定路径不存在，则从默认位置获取
+        if (!FileUtil.exist(configFilePath)) {
+            // 用户配置文件默认路径
+            String defaultPath = System.getProperty("user.dir") + File.separator + "config.ini";
+            // 若默认路径也不存在，则抛出 FileNotFoundException
+            return Props.getProp(defaultPath, StandardCharsets.UTF_8);
         }
+
+        Path absolutePath = Paths.get(configFilePath).toAbsolutePath();
+
+        return Props.getProp(absolutePath.toString(), StandardCharsets.UTF_8);
     }
+
 }
