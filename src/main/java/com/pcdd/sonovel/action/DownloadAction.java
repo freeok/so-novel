3e802743b305b7cc7bd7aebf4a3bb53bed130f8e
freeok <51998152+freeok@users.noreply.github.com>
2024-11-11 20:53:19 +0800

    :bug: 修复中文输入乱码(Scanner、LineReader混用)

--- src/main/java/com/pcdd/sonovel/action/DownloadAction.java
+++ src/main/java/com/pcdd/sonovel/action/DownloadAction.java
@@ -5,42 +5,39 @@ import cn.hutool.core.lang.ConsoleTable;
 import cn.hutool.core.util.NumberUtil;
 import com.pcdd.sonovel.core.Crawler;
 import com.pcdd.sonovel.model.SearchResult;
+import lombok.SneakyThrows;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 
 import java.util.List;
-import java.util.Scanner;
 
 import static org.fusesource.jansi.AnsiRenderer.render;
 
 public class DownloadAction implements Action {
 
+    @SneakyThrows
     @Override
     public void execute(Terminal terminal) {
-        Scanner scanner = Console.scanner();
-        Console.log(render("==> @|blue 请输入书名或作者：|@"));
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
         // 1. 查询
-        String keyword = scanner.nextLine().trim();
-        if (keyword.isEmpty()) {
-            return;
-        }
+        String keyword = reader.readLine(render("==> @|blue 请输入书名或作者: |@")).trim();
+        if (keyword.isEmpty()) return;
         List<SearchResult> results = Crawler.search(keyword);
-        if (results.isEmpty()) {
-            return;
-        }
-        // 2. 打印搜索结果
-        printSearchResult(results);
+        if (results.isEmpty()) return;
 
-        // 3. 选择后下载
-        Console.log("==> 请输入下载序号（首列的数字）");
-        int num = Integer.parseInt(scanner.nextLine());
+        // 2. 打印搜索结果
+        printSearchResult(Crawler.search(keyword));
+
+        // 3. 选择下载章节
+        int num = Integer.parseInt(reader.readLine("==> 请输入下载序号（首列的数字）"));
         Console.log("==> 0: 下载全本");
         Console.log("==> 1: 下载指定章节");
-        int downloadPolicy = Integer.parseInt(scanner.nextLine());
+        int downloadPolicy = Integer.parseInt(reader.readLine(""));
         int start = 1;
         int end = Integer.MAX_VALUE;
         if (downloadPolicy == 1) {
-            Console.log("==> 请输起始章(最小为1)和结束章，用空格隔开");
-            String[] split = scanner.nextLine().split("\\s+");
+            String[] split = reader.readLine("==> 请输起始章(最小为1)和结束章，用空格隔开").split("\\s+");
             start = Integer.parseInt(split[0]);
             end = Integer.parseInt(split[1]);
         }
