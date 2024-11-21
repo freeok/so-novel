dcc18b740ed28a24064ff6d08f5e4afff483656b
freeok <51998152+freeok@users.noreply.github.com>
2024-11-22 00:42:29 +0800

    Add ExceptionUtils.java

---
+++ src/main/java/com/pcdd/sonovel/util/ExceptionUtils.java
@@ -0,0 +1,13 @@
+package com.pcdd.sonovel.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ExceptionUtils {
+
+    // 随机抛异常，测试用
+    public void randomThrow() {
+        int i = System.currentTimeMillis() % 2 == 0 ? 0 : 1 / 0;
+    }
+
+}
