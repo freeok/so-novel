ddccbd7c19d5db19925fe684dd52384f7c8ff6ac
freeok <51998152+freeok@users.noreply.github.com>
2024-11-26 01:42:49 +0800

    :construction: 新增书源2中

--- src/main/java/com/pcdd/sonovel/parse/SearchResultParser.java
+++ src/main/java/com/pcdd/sonovel/parse/SearchResultParser.java
@@ -1,16 +1,20 @@
 package com.pcdd.sonovel.parse;
 
+import cn.hutool.core.lang.Validator;
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.json.JSONUtil;
 import com.pcdd.sonovel.model.Rule;
 import com.pcdd.sonovel.model.SearchResult;
 import lombok.SneakyThrows;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 /**
@@ -27,27 +31,33 @@ public class SearchResultParser extends Parser {
     @SneakyThrows
     public List<SearchResult> parse(String keyword) {
         Rule.Search search = this.rule.getSearch();
-        Connection connect = Jsoup.connect(search.getUrl()).timeout(TIMEOUT_MILLS);
-        // 搜索结果页DOM
-        Document document = connect.data(search.getBody().getKeyword(), keyword).post();
+        // 搜索结果页 DOM
+        Document document = Jsoup.connect(search.getUrl())
+                .timeout(TIMEOUT_MILLS)
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36")
+                .data(buildParams(keyword))
+                .cookies(buildCookies())
+                .post();
         Elements elements = document.select(search.getResult());
 
         List<SearchResult> list = new ArrayList<>();
         for (Element element : elements) {
             // jsoup 不支持一次性获取属性的值
-            String url = element.select(search.getBookName()).attr("href");
+            String href = element.select(search.getBookName()).attr("href");
             String bookName = element.select(search.getBookName()).text();
             String latestChapter = element.select(search.getLatestChapter()).text();
             String author = element.select(search.getAuthor()).text();
             String update = element.select(search.getUpdate()).text();
 
-            // 排除第一个 tr（表头）
+            // 针对书源 1：排除第一个 tr（表头）
             // 如果存在任何一个字符串为空字符串，则执行相应的操作
-            if (Stream.of(url, bookName, latestChapter, author, update).anyMatch(String::isEmpty)) {
-                continue;
-            }
+            if (Stream.of(href, bookName, latestChapter, author, update).anyMatch(String::isEmpty)) continue;
+
+            // 有的 href 是相对路径，需要拼接为完整路径
+            href = Validator.isUrl(href) ? href : URLUtil.normalize(this.rule.getUrl() + href);
+
             SearchResult build = SearchResult.builder()
-                    .url(url)
+                    .url(href)
                     .bookName(bookName)
                     .latestChapter(latestChapter)
                     .author(author)
@@ -58,6 +68,27 @@ public class SearchResultParser extends Parser {
         }
 
         return list;
+    }
+
+    private Map<String, String> buildParams(String keyword) {
+        Map<String, String> params = new HashMap<>();
+
+        JSONUtil.parseObj(this.rule.getSearch().getBody())
+                .forEach((key, value) -> {
+                    if (key.equals("kw")) params.put(value.toString(), keyword);
+                    else params.put(key, value.toString());
+                });
+
+        return params;
+    }
+
+    private Map<String, String> buildCookies() {
+        Map<String, String> cookies = new HashMap<>();
+
+        JSONUtil.parseObj(this.rule.getSearch().getCookies())
+                .forEach((key, value) -> cookies.put(key, value.toString()));
+
+        return cookies;
     }
 
 
