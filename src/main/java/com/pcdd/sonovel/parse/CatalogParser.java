a3248f2e65dd4471efb84f7fc3ae100f6d610ba3
freeok <51998152+freeok@users.noreply.github.com>
2024-11-25 21:55:41 +0800

    :boom: 新增字段Rule.Book.catalogOffset

--- src/main/java/com/pcdd/sonovel/parse/CatalogParser.java
+++ src/main/java/com/pcdd/sonovel/parse/CatalogParser.java
@@ -1,30 +1,23 @@
 package com.pcdd.sonovel.parse;
 
 import cn.hutool.core.util.URLUtil;
-import cn.hutool.setting.dialect.Props;
 import com.pcdd.sonovel.model.Chapter;
-import com.pcdd.sonovel.util.Settings;
+import com.pcdd.sonovel.model.Rule;
 import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author pcdd
  */
 public class CatalogParser extends Parser {
 
-    public static final String INDEX_URL;
     private static final int TIMEOUT_MILLS = 30_000;
-
-    // 加载配置文件参数
-    static {
-        Props sys = Settings.sys();
-        INDEX_URL = sys.getStr("index_url");
-    }
 
     public CatalogParser(int sourceId) {
         super(sourceId);
@@ -42,14 +35,22 @@ public class CatalogParser extends Parser {
      */
     @SneakyThrows
     public List<Chapter> parse(String url, int start, int end) {
-        Document document = Jsoup.parse(URLUtil.url(url), TIMEOUT_MILLS);
-        Elements elements = document.select(this.rule.getBook().getCatalog());
-        List<Chapter> catalog = new ArrayList<>();
+        Rule.Book book = this.rule.getBook();
+        // 正数表示忽略前 offset 章，负数表示忽略后 offset 章
+        int offset = Optional.ofNullable(book.getCatalogOffset()).orElse(0);
 
+        Document document = Jsoup.parse(URLUtil.url(url), TIMEOUT_MILLS);
+        List<Element> elements = document.select(book.getCatalog());
+        if (offset != 0) {
+            if (offset > 0) elements = elements.subList(offset, elements.size());
+            if (offset < 0) elements = elements.subList(0, elements.size() + offset);
+        }
+
+        List<Chapter> catalog = new ArrayList<>();
         for (int i = start - 1; i < end && i < elements.size(); i++) {
             Chapter build = Chapter.builder()
                     .title(elements.get(i).text())
-                    .url(INDEX_URL + elements.get(i).attr("href"))
+                    .url(this.rule.getUrl() + elements.get(i).attr("href"))
                     .chapterNo(i + 1)
                     .build();
             catalog.add(build);
