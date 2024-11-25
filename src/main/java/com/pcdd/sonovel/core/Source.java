1ac7951bad7cfd1e9a1eff0ca43d5fdc2ec162dd
freeok <51998152+freeok@users.noreply.github.com>
2024-11-26 03:56:00 +0800

    :recycle: 抽象类Parser重命名为Source，新增常量类

--- src/main/java/com/pcdd/sonovel/core/Source.java
+++ src/main/java/com/pcdd/sonovel/core/Source.java
@@ -1,4 +1,4 @@
-package com.pcdd.sonovel.parse;
+package com.pcdd.sonovel.core;
 
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.json.JSONUtil;
@@ -7,11 +7,11 @@ import com.pcdd.sonovel.model.Rule;
 /**
  * @author pcdd
  */
-public abstract class Parser {
+public abstract class Source {
 
     public final Rule rule;
 
-    protected Parser(int sourceId) {
+    protected Source(int sourceId) {
         // 根据 ruleId 获取对应 json 文件内容
         String jsonStr = ResourceUtil.readUtf8Str("rule/rule" + sourceId + ".json");
         // json 封装进 Rule
