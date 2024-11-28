1256d64b90aba343bef64d28a34f5e76dc394e3e
freeok <51998152+freeok@users.noreply.github.com>
2024-11-28 16:43:17 +0800

    :recycle: 重命名ConfigConst.java

--- src/main/java/com/pcdd/sonovel/core/Crawler.java
+++ src/main/java/com/pcdd/sonovel/core/Crawler.java
@@ -26,7 +26,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static com.pcdd.sonovel.util.ConfigConsts.*;
+import static com.pcdd.sonovel.util.ConfigConst.*;
 import static org.fusesource.jansi.AnsiRenderer.render;
 
 /**
