bea6f304af160d4fbc2149655a58546a28edf692
freeok <51998152+freeok@users.noreply.github.com>
2024-11-28 16:34:06 +0800

    Add CrawlUtils.java

---
+++ src/main/java/com/pcdd/sonovel/util/CrawlUtils.java
@@ -0,0 +1,61 @@
+package com.pcdd.sonovel.util;
+
+import cn.hutool.core.lang.Validator;
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.json.JSONUtil;
+import lombok.experimental.UtilityClass;
+import org.jsoup.Connection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@UtilityClass
+public class CrawlUtils {
+
+    // 有的 href 是相对路径，需要拼接为完整路径
+    public String normalizeUrl(String href, String indexUrl) {
+        return Validator.isUrl(href) ? href : URLUtil.normalize(indexUrl + href);
+    }
+
+    public Map<String, String> buildParams(String body, String keyword) {
+        Map<String, String> params = new HashMap<>();
+
+        JSONUtil.parseObj(body)
+                .forEach((key, value) -> {
+                    if (key.equals("kw")) params.put(value.toString(), keyword);
+                    else params.put(key, value.toString());
+                });
+
+        return params;
+    }
+
+    public Map<String, String> buildCookies(String cookies) {
+        Map<String, String> map = new HashMap<>();
+
+        JSONUtil.parseObj(cookies)
+                .forEach((key, value) -> map.put(key, value.toString()));
+
+        return map;
+    }
+
+    public Connection.Method buildMethod(String method) {
+        return switch (method.toLowerCase()) {
+            case "get" -> Connection.Method.GET;
+            case "post" -> Connection.Method.POST;
+            case "put" -> Connection.Method.PUT;
+            case "delete" -> Connection.Method.DELETE;
+            case "patch" -> Connection.Method.PATCH;
+            case "head" -> Connection.Method.HEAD;
+            case "options" -> Connection.Method.OPTIONS;
+            case "trace" -> Connection.Method.TRACE;
+            default -> Connection.Method.POST;
+        };
+    }
+
+    public void randomSleep(long min, long max) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextLong(min, max));
+    }
+
+}
