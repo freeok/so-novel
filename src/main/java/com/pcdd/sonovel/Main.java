20a1e179f3803502b7694651785e6f15d7977ce8
freeok <51998152+freeok@users.noreply.github.com>
2024-11-30 00:20:17 +0800

    :boom: 重构动态加载配置文件

--- src/main/java/com/pcdd/sonovel/Main.java
+++ src/main/java/com/pcdd/sonovel/Main.java
@@ -5,9 +5,12 @@ import cn.hutool.core.lang.ConsoleTable;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.log.dialect.console.ConsoleLog;
 import cn.hutool.log.level.Level;
+import cn.hutool.setting.Setting;
 import com.pcdd.sonovel.action.CheckUpdateAction;
 import com.pcdd.sonovel.action.DownloadAction;
+import com.pcdd.sonovel.model.ConfigBean;
 import com.pcdd.sonovel.parse.BookParser;
+import com.pcdd.sonovel.util.ConfigUtils;
 import lombok.SneakyThrows;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
@@ -15,9 +18,9 @@ import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
+import java.io.File;
 import java.util.List;
 
-import static com.pcdd.sonovel.util.ConfigConst.*;
 import static org.fusesource.jansi.AnsiRenderer.render;
 
 /**
@@ -29,9 +32,13 @@ import static org.fusesource.jansi.AnsiRenderer.render;
  */
 public class Main {
 
+    private static volatile ConfigBean config = ConfigUtils.config();
+
     @SneakyThrows
     public static void main(String[] args) {
-        ConsoleLog.setLevel(Level.OFF);
+        ConsoleLog.setLevel(Level.ALL);
+
+        init();
 
         List<String> options = List.of("1.下载小说", "2.检查更新", "3.查看配置文件", "4.使用须知", "5.结束程序");
         Terminal terminal = TerminalBuilder.builder()
@@ -51,13 +58,13 @@ public class Main {
                 Console.error("无效的选项，请重新选择");
             }
             if (options.get(0).equals(cmd)) {
-                new DownloadAction().execute(terminal);
+                new DownloadAction(config).execute(terminal);
             }
             if (options.get(1).equals(cmd)) {
-                new CheckUpdateAction().execute(terminal);
+                new CheckUpdateAction().execute();
             }
             if (options.get(2).equals(cmd)) {
-                Console.log(usr);
+                Console.log(config);
             }
             if (options.get(3).equals(cmd)) {
                 printHint();
@@ -76,10 +83,10 @@ public class Main {
         Console.table(ConsoleTable.create()
                 // 是否转为全角
                 .setSBCMode(false)
-                .addHeader(render(StrUtil.format("@|BG_blue,ITALIC,BOLD  so-novel v{} |@", VERSION)) + "（本项目开源且免费）")
+                .addHeader(render(StrUtil.format("@|BG_blue,ITALIC,BOLD  so-novel v{} |@", config.getVersion())) + "（本项目开源且免费）")
                 .addHeader("官方地址：https://github.com/freeok/so-novel")
-                .addHeader(StrUtil.format("当前书源：{} (ID: {})", new BookParser(SOURCE_ID).rule.getUrl(), SOURCE_ID))
-                .addHeader(render("导出格式：@|blue " + EXT_NAME + "|@"))
+                .addHeader(StrUtil.format("当前书源：{} (ID: {})", new BookParser(config.getSourceId()).rule.getUrl(), config.getSourceId()))
+                .addHeader(render("导出格式：@|blue " + config.getExtName() + "|@"))
                 .addBody("使用须知")
                 .addBody("1. 请按要求操作，然后按 Enter 键执行")
                 .addBody("2. 下载受书源、网络环境等因素影响。若出现 xx timed out，建议检查网络环境或稍后再试")
@@ -87,6 +94,16 @@ public class Main {
                 .addBody("4. 若认为下载速度较慢，可适当减小爬取间隔，直至合适为止")
                 .addBody("5. 爬取间隔过小会导致当前 IP 被有反爬机制的书源限流，可能短时间内将无法使用该书源")
         );
+    }
+
+    private static void init() {
+        String path = System.getProperty("user.dir") + File.separator + "config.ini";
+        Setting setting = new Setting(path);
+        // 监听配置文件
+        setting.autoLoad(true, aBoolean -> {
+            Console.log("监听到配置文件修改！");
+            config = ConfigUtils.config();
+        });
     }
 
 }
