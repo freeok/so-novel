d0d2964aaf8d294e17ace32e4cad5b1ce978c885
freeok <51998152+freeok@users.noreply.github.com>
2024-12-04 03:54:05 +0800

    :children_crossing: 启动校验source-id合法性

--- src/main/java/com/pcdd/sonovel/core/Source.java
+++ src/main/java/com/pcdd/sonovel/core/Source.java
@@ -1,8 +1,11 @@
 package com.pcdd.sonovel.core;
 
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.lang.Console;
 import cn.hutool.json.JSONUtil;
 import com.pcdd.sonovel.model.Rule;
+
+import static org.jline.jansi.AnsiRenderer.render;
 
 /**
  * @author pcdd
@@ -12,9 +15,18 @@ public class Source {
 
     public final Rule rule;
 
-    public Source(int sourceId) {
-        // 根据 sourceId 获取对应书源规则
-        String jsonStr = ResourceUtil.readUtf8Str("rule/rule" + sourceId + ".json");
+    public Source(int id) {
+        String jsonStr = null;
+
+        try {
+            // 根据 sourceId 获取对应书源规则
+            jsonStr = ResourceUtil.readUtf8Str("rule/rule" + id + ".json");
+        } catch (Exception e) {
+            Console.error(render("@|red 书源规则初始化失败，请检查配置项 source-id|@"));
+            Console.error(render("@|red 错误信息：{}|@"), e.getMessage());
+            System.exit(1);
+        }
+
         // json 封装进 Rule
         this.rule = JSONUtil.toBean(jsonStr, Rule.class);
     }
