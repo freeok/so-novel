711bee4fd19fbea70c7c44bae6e219cc8890a514
freeok <51998152+freeok@users.noreply.github.com>
2024-12-04 02:54:37 +0800

    :children_crossing: 改善交互体验 (https://github.com/freeok/so-novel/issues/46)

--- src/main/java/com/pcdd/sonovel/action/DownloadAction.java
+++ src/main/java/com/pcdd/sonovel/action/DownloadAction.java
@@ -28,6 +28,7 @@ public class DownloadAction {
     @SneakyThrows
     public void execute(Terminal terminal) {
         LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+
         // 1. 查询
         String keyword = reader.readLine(render("==> @|blue 请输入书名或作者（宁少字别错字）: |@")).trim();
         if (keyword.isEmpty()) return;
@@ -38,30 +39,43 @@ public class DownloadAction {
         printSearchResult(results);
 
         int num;
-        int downloadPolicy = 2;
-        SearchResult sr = null;
+        int action;
+        SearchResult sr;
         // 3. 选择下载章节
-        do {
-            String input = reader.readLine("==> 请输入下载序号（首列的数字）").trim();
+        while (true) {
+            String input = reader.readLine("==> 请输入下载序号（首列的数字，或输入 0 返回）：").trim();
             // 健壮性判断：必须为数字
             try {
                 num = Integer.parseInt(input);
             } catch (NumberFormatException e) {
                 continue;
             }
-            // 健壮性判断：必须为首列的数字
-            if (num < 0 || num >= results.size()) continue;
-            sr = results.get(num);
+
+            // 没有搜到想要的书，返回
+            if (num == 0) return;
+            // 健壮性判断：必须为首列的序号
+            if (num < 0 || num > results.size()) continue;
+
+            sr = results.get(num - 1);
             Console.log("<== 你选择了《{}》({})", sr.getBookName(), sr.getAuthor());
-            Console.log("==> 0: 下载全本");
-            Console.log("==> 1: 下载指定章节");
-            Console.log("==> 2: 重新选择");
-            downloadPolicy = Integer.parseInt(reader.readLine("==> 请输入数字："));
-        } while (downloadPolicy == 2);
+            Console.log("==> 0: 重新选择功能");
+            Console.log("==> 1: 下载全本");
+            Console.log("==> 2: 下载指定章节");
+            Console.log("==> 3: 重新输入序号");
+
+            try {
+                action = Integer.parseInt(reader.readLine("==> 请输入数字："));
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+            if (action != 3) break;
+        }
+        if (action == 0) return;
 
         int start = 1;
         int end = Integer.MAX_VALUE;
-        if (downloadPolicy == 1) {
+        if (action == 2) {
             String[] split = reader.readLine("==> 请输起始章(最小为1)和结束章，用空格隔开：").trim().split("\\s+");
             start = Integer.parseInt(split[0]);
             end = Integer.parseInt(split[1]);
@@ -72,8 +86,8 @@ public class DownloadAction {
 
     private static void printSearchResult(List<SearchResult> results) {
         ConsoleTable consoleTable = ConsoleTable.create().addHeader("序号", "书名", "作者", "最新章节", "最后更新时间");
-        for (int i = 0; i < results.size(); i++) {
-            SearchResult r = results.get(i);
+        for (int i = 1; i <= results.size(); i++) {
+            SearchResult r = results.get(i - 1);
             consoleTable.addBody(String.valueOf(i),
                     r.getBookName(),
                     r.getAuthor(),
