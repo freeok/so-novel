825ed94d89aef6822f3a14c251bb466186b7a2ca
freeok <51998152+freeok@users.noreply.github.com>
2024-12-04 06:35:56 +0800

    :sparkles: 优化epub、html正文排版

--- src/main/java/com/pcdd/sonovel/core/ChapterFilter.java
+++ src/main/java/com/pcdd/sonovel/core/ChapterFilter.java
@@ -56,13 +56,15 @@ public class ChapterFilter extends Source {
          */
         public String build() {
             if (applyCharacterFilter) {
-                // 替换非法的 &..;（HTML字符实体引用），可能会导致ibooks章节报错
+                // 替换非法的 &..;（HTML 字符实体引用），可能会导致 ibooks 章节报错
                 this.content = this.content.replaceAll("&[^;]+;", "");
             }
+
             if (applyAdFilter) {
                 String filteredContent = this.content.replaceAll(rule.getChapter().getFilterTxt(), "");
                 this.content = HtmlUtil.removeHtmlTag(filteredContent, StrUtil.splitToArray(rule.getChapter().getFilterTag(), " "));
             }
+
             return this.content;
         }
     }
