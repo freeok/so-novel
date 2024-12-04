db6b70ebd5898e52fbde1e93e53e777c53edfd26
freeok <51998152+freeok@users.noreply.github.com>
2024-12-04 09:13:25 +0800

    :recycle: 解耦ChapterConverter.java

---
+++ src/main/java/com/pcdd/sonovel/core/ChapterFormatter.java
@@ -0,0 +1,41 @@
+package com.pcdd.sonovel.core;
+
+import com.pcdd.sonovel.model.ConfigBean;
+import com.pcdd.sonovel.model.Rule;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pcdd
+ * Created at 2024/12/4
+ */
+@AllArgsConstructor
+public class ChapterFormatter {
+
+    private final ConfigBean config;
+
+    public String format(String content) {
+        Rule.Chapter rule = new Source(config.getSourceId()).rule.getChapter();
+
+        // 标签闭合
+        if (Boolean.TRUE.equals(rule.getParagraphTagClosed())) {
+            // <p>段落</p>
+            if ("p".equals(rule.getParagraphTag())) {
+                return content;
+            } else { // 非<p>的闭合标签，替换为<p>标签
+                return content.replaceAll("<(?!p\\b)([^>]+)>(.*?)</\\1>", "<p>$2</p>");
+            }
+        }
+        // 标签不闭合，用某个标签分隔，例如：段落1<br><br>段落2
+        String tag = rule.getParagraphTag();
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : content.replaceAll("\\s+", "").split(tag)) {
+            if (!s.isBlank()) {
+                sb.append("<p>").append(s).append("</p>");
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
