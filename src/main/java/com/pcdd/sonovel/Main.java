9a6ed0887951ebce1618dcf06ed41d7a41925793
freeok <51998152+freeok@users.noreply.github.com>
2024-12-09 09:04:10 +0800

    :memo: Update Javadoc: Main.java

--- src/main/java/com/pcdd/sonovel/Main.java
+++ src/main/java/com/pcdd/sonovel/Main.java
@@ -29,8 +29,9 @@ import static org.fusesource.jansi.AnsiRenderer.render;
  * @author pcdd
  * Created at 2021/6/10
  * <p>
- * IDEA 的终端无法开发 jline 程序，因此在 wt 中运行
- * 启动命令：mvnd clean compile; mvn exec:java
+ * {@code mvnd clean compile && mvn exec:java}
+ * <p>
+ * {@code mvnd clean compile; mvn exec:java}
  */
 public class Main {
 
