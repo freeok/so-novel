84f2d39621772a8b867680732468eeb8f5928f56
freeok <51998152+freeok@users.noreply.github.com>
2024-12-23 10:07:31 +0800

    :sparkles: 为txt、html格式增加封面

--- src/main/java/com/pcdd/sonovel/handle/HtmlCatalogHandler.java
+++ src/main/java/com/pcdd/sonovel/handle/HtmlCatalogHandler.java
@@ -5,6 +5,7 @@ import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.io.file.FileWriter;
 import cn.hutool.core.util.ReUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
 import com.pcdd.sonovel.model.Book;
 import com.pcdd.sonovel.util.FileUtils;
 
@@ -20,16 +21,15 @@ import java.util.List;
 public class HtmlCatalogHandler implements PostProcessingHandler {
 
     @Override
-    public void handle(Book b, File saveDir) {
+    public void handle(Book book, File saveDir) {
         List<String> strings = new ArrayList<>();
-        List<File> files = FileUtils.sortFilesByName(saveDir);
         String regex = "<title>(.*?)</title>";
 
         strings.add("文件名\t\t章节名");
         int i = 1;
-        for (File file : files) {
+        for (File file : FileUtils.sortFilesByName(saveDir)) {
             FileReader fr = FileReader.create(file, StandardCharsets.UTF_8);
-            // 获取 title 标签内容
+            // 获取 <title> 内容
             String title = ReUtil.getGroup1(regex, fr.readString());
             strings.add(StrUtil.format("{}_.html\t\t{}", i++, title));
         }
@@ -37,7 +37,10 @@ public class HtmlCatalogHandler implements PostProcessingHandler {
         File file = FileUtil.touch(saveDir + File.separator, "0_目录.txt");
         FileWriter fw = FileWriter.create(file, StandardCharsets.UTF_8);
         fw.writeLines(strings);
+
+        // 下载封面
+        File coverFile = HttpUtil.downloadFileFromUrl(book.getCoverUrl(), System.getProperty("user.dir") + File.separator + saveDir);
+        FileUtil.rename(coverFile, "0_封面." + FileUtil.getType(coverFile), true);
     }
 
 }
-
