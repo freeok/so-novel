af00f4171749f9998679655c16ca3a9580a37b13
freeok <51998152+freeok@users.noreply.github.com>
2024-12-23 10:07:31 +0800

    :sparkles: 为txt、html格式增加封面

--- src/main/java/com/pcdd/sonovel/handle/TxtMergeHandler.java
+++ src/main/java/com/pcdd/sonovel/handle/TxtMergeHandler.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileAppender;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HtmlUtil;
+import cn.hutool.http.HttpUtil;
 import com.pcdd.sonovel.model.Book;
 import com.pcdd.sonovel.model.ConfigBean;
 import com.pcdd.sonovel.util.FileUtils;
@@ -22,17 +23,17 @@ public class TxtMergeHandler implements PostProcessingHandler {
     private final ConfigBean config;
 
     @Override
-    public void handle(Book b, File saveDir) {
+    public void handle(Book book, File saveDir) {
         String outputPath = StrUtil.format("{}{}{}（{}）.txt",
                 System.getProperty("user.dir") + File.separator, config.getDownloadPath() + File.separator,
-                b.getBookName(), b.getAuthor());
+                book.getBookName(), book.getAuthor());
         File outputFile = FileUtil.touch(outputPath);
         FileAppender appender = new FileAppender(outputFile, 16, true);
 
         List<String> format = List.of(
-                StrUtil.format("书名：{}", b.getBookName()),
-                StrUtil.format("作者：{}", b.getAuthor()),
-                StrUtil.format("简介：{}", StrUtil.isEmpty(b.getIntro()) ? "暂无" : HtmlUtil.cleanHtmlTag(b.getIntro())),
+                StrUtil.format("书名：{}", book.getBookName()),
+                StrUtil.format("作者：{}", book.getAuthor()),
+                StrUtil.format("简介：{}", StrUtil.isEmpty(book.getIntro()) ? "暂无" : HtmlUtil.cleanHtmlTag(book.getIntro())),
                 StrUtil.format("{}", "\u3000".repeat(2))
         );
         // 首页添加书籍信息
@@ -41,8 +42,11 @@ public class TxtMergeHandler implements PostProcessingHandler {
         for (File f : FileUtils.sortFilesByName(saveDir)) {
             appender.append(FileUtil.readUtf8String(f));
         }
-
         appender.flush();
+
+        // 下载封面
+        File coverFile = HttpUtil.downloadFileFromUrl(book.getCoverUrl(), System.getProperty("user.dir") + File.separator + saveDir);
+        FileUtil.rename(coverFile, "0_封面." + FileUtil.getType(coverFile), true);
     }
 
 }
