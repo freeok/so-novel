a61bab53d6cfae2b7d574340434862ab064b50e1
freeok <51998152+freeok@users.noreply.github.com>
2024-12-28 16:21:20 +0800

    :bug: 修复部分书源搜索结果唯一时，搜索结果为空的问题（直接进入了详情页）

--- src/main/java/com/pcdd/sonovel/parse/SearchResultParser.java
+++ src/main/java/com/pcdd/sonovel/parse/SearchResultParser.java
@@ -5,12 +5,14 @@ import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.core.util.URLUtil;
 import com.pcdd.sonovel.core.Source;
+import com.pcdd.sonovel.model.Book;
 import com.pcdd.sonovel.model.ConfigBean;
 import com.pcdd.sonovel.model.Rule;
 import com.pcdd.sonovel.model.SearchResult;
 import com.pcdd.sonovel.util.CrawlUtils;
 import lombok.SneakyThrows;
 import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -38,18 +40,19 @@ public class SearchResultParser extends Source {
 
         // 模拟搜索请求
         Document document;
+        Connection.Response resp;
         try {
-            Connection.Response resp = getConn(search.getUrl(), TIMEOUT_MILLS)
+            resp = getConn(search.getUrl(), TIMEOUT_MILLS)
                     .data(CrawlUtils.buildParams(this.rule.getSearch().getBody(), keyword))
                     .cookies(CrawlUtils.buildCookies(this.rule.getSearch().getCookies()))
                     .execute();
-            document = resp.parse();
+            document = Jsoup.parse(resp.body());
         } catch (Exception e) {
             Console.error(e.getMessage());
             return Collections.emptyList();
         }
 
-        List<SearchResult> firstPageResults = getSearchResults(null, document);
+        List<SearchResult> firstPageResults = getSearchResults(null, resp);
         if (!search.isPagination()) return firstPageResults;
 
         Set<String> urls = new LinkedHashSet<>();
@@ -69,19 +72,35 @@ public class SearchResultParser extends Source {
     }
 
     @SneakyThrows
-    private List<SearchResult> getSearchResults(String url, Document document) {
+    private List<SearchResult> getSearchResults(String url, Connection.Response resp) {
         Rule.Search rule = this.rule.getSearch();
+        List<SearchResult> list = new ArrayList<>();
         // 搜索结果页 DOM
-        if (document == null)
-            document = getConn(url, TIMEOUT_MILLS).get();
+        Document document = resp == null ? getConn(url, TIMEOUT_MILLS).get() : Jsoup.parse(resp.body());
+
+        // 部分书源完全匹配会直接进入详情页，因此需要构造搜索结果
+        if (document.select(rule.getResult()).isEmpty()) {
+            String bookUrl = resp.url().toString();
+            BookParser bookParser = new BookParser(config);
+            Book book = bookParser.parse(bookUrl);
+            SearchResult build = SearchResult.builder()
+                    .url(bookUrl)
+                    .bookName(book.getBookName())
+                    .author(book.getAuthor())
+                    .latestChapter(book.getLatestChapter())
+                    .latestUpdate(book.getLatestUpdate())
+                    .build();
+            list.add(build);
+            return list;
+        }
 
         Elements elements = document.select(rule.getResult());
-        List<SearchResult> list = new ArrayList<>();
         for (Element element : elements) {
             // jsoup 不支持一次性获取属性的值
             String href = element.select(rule.getBookName()).attr("href");
             String bookName = element.select(rule.getBookName()).text();
             String author = element.select(rule.getAuthor()).text();
+            // 以下为非必须属性，需判空，否则抛出 org.jsoup.helper.ValidationException: String must not be empty
             String latestChapter = StrUtil.isNotEmpty(rule.getLatestChapter())
                     ? element.select(rule.getLatestChapter()).text()
                     : null;
