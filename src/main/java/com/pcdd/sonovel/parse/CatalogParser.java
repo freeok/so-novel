90b67919d165d5f4ae275225a153dacd40d4bda8
freeok <51998152+freeok@users.noreply.github.com>
2024-12-28 13:09:57 +0800

    :construction: 适配69书吧：目录解析

--- src/main/java/com/pcdd/sonovel/parse/CatalogParser.java
+++ src/main/java/com/pcdd/sonovel/parse/CatalogParser.java
@@ -1,11 +1,15 @@
 package com.pcdd.sonovel.parse;
 
-import cn.hutool.core.util.URLUtil;
+import cn.hutool.core.util.ReUtil;
+import cn.hutool.core.util.StrUtil;
 import com.pcdd.sonovel.core.Source;
 import com.pcdd.sonovel.model.Chapter;
+import com.pcdd.sonovel.model.ConfigBean;
 import com.pcdd.sonovel.model.Rule;
 import com.pcdd.sonovel.util.CrawlUtils;
+import com.pcdd.sonovel.util.RandomUA;
 import lombok.SneakyThrows;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,9 +25,11 @@ import java.util.Optional;
 public class CatalogParser extends Source {
 
     private static final int TIMEOUT_MILLS = 30_000;
+    private final ConfigBean config;
 
-    public CatalogParser(int sourceId) {
-        super(sourceId);
+    public CatalogParser(ConfigBean config) {
+        super(config.getSourceId());
+        this.config = config;
     }
 
     /**
@@ -38,12 +44,19 @@ public class CatalogParser extends Source {
      */
     @SneakyThrows
     public List<Chapter> parse(String url, int start, int end) {
-        Rule.Book book = this.rule.getBook();
+        Rule.Catalog catalogRule = this.rule.getCatalog();
+        Rule.Book bookRule = this.rule.getBook();
+        // 目录和详情不一定在同一页面
+        if (StrUtil.isNotEmpty(catalogRule.getUrl())) {
+            // 提取 url 中的变量
+            String id = ReUtil.get(bookRule.getUrl(), url, 1);
+            url = catalogRule.getUrl().formatted(id);
+        }
         // 正数表示忽略前 offset 章，负数表示忽略后 offset 章
-        int offset = Optional.ofNullable(book.getCatalogOffset()).orElse(0);
+        int offset = Optional.ofNullable(catalogRule.getOffset()).orElse(0);
 
-        Document document = Jsoup.parse(URLUtil.url(url), TIMEOUT_MILLS);
-        List<Element> elements = document.select(book.getCatalog());
+        Document document = getConn(url).get();
+        List<Element> elements = document.select(catalogRule.getResult());
         if (offset != 0) {
             if (offset > 0) elements = elements.subList(offset, elements.size());
             if (offset < 0) elements = elements.subList(0, elements.size() + offset);
@@ -60,6 +73,22 @@ public class CatalogParser extends Source {
         }
 
         return catalog;
+    }
+
+    private Connection getConn(String url) {
+        Connection conn = Jsoup.connect(url)
+                .method(CrawlUtils.buildMethod(this.rule.getSearch().getMethod()))
+                .header("User-Agent", RandomUA.generate())
+                .timeout(TIMEOUT_MILLS);
+
+        // 启用配置文件的代理地址
+        if (this.rule.isUseProxy()) {
+            String proxyServer = config.getProxyServer();
+            String[] split = proxyServer.split(":");
+            conn.proxy(split[0], Integer.parseInt(split[1]));
+        }
+
+        return conn;
     }
 
 }
