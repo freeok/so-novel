8c4d28545df1a475ca21443c3db39ec71bbdd79a
freeok <51998152+freeok@users.noreply.github.com>
2025-01-06 02:59:21 +0800

    :sparkles: 兼容目录分页的书源

--- src/main/java/com/pcdd/sonovel/parse/CatalogParser.java
+++ src/main/java/com/pcdd/sonovel/parse/CatalogParser.java
@@ -1,10 +1,11 @@
 package com.pcdd.sonovel.parse;
 
+import cn.hutool.core.lang.Validator;
 import cn.hutool.core.util.ReUtil;
 import cn.hutool.core.util.StrUtil;
 import com.pcdd.sonovel.core.Source;
-import com.pcdd.sonovel.model.Chapter;
 import com.pcdd.sonovel.model.AppConfig;
+import com.pcdd.sonovel.model.Chapter;
 import com.pcdd.sonovel.model.Rule;
 import com.pcdd.sonovel.util.CrawlUtils;
 import lombok.SneakyThrows;
@@ -44,27 +45,47 @@ public class CatalogParser extends Source {
         // 目录和详情不一定在同一页面
         if (StrUtil.isNotEmpty(catalogRule.getUrl())) {
             // 提取 url 中的变量
-            String id = ReUtil.get(bookRule.getUrl(), url, 1);
+            String id = ReUtil.getGroup1(bookRule.getUrl(), url);
             url = catalogRule.getUrl().formatted(id);
         }
         // 正数表示忽略前 offset 章，负数表示忽略后 offset 章
         int offset = Optional.ofNullable(catalogRule.getOffset()).orElse(0);
-
         Document document = getConn(url, TIMEOUT_MILLS).get();
-        List<Element> elements = document.select(catalogRule.getResult());
-        if (offset != 0) {
-            if (offset > 0) elements = elements.subList(offset, elements.size());
-            if (offset < 0) elements = elements.subList(0, elements.size() + offset);
+        List<String> urls = new ArrayList<>();
+
+        // 优化后的代码
+        if (catalogRule.isPagination()) {
+            while (true) {
+                String href = document.select(catalogRule.getNextPage()).attr("href");
+                // 判断 href 是否是有效的 URL
+                if (!(Validator.isUrl(href) || StrUtil.startWith(href, "/"))) break;
+                // 规范化 URL 并添加到列表
+                urls.add(CrawlUtils.normalizeUrl(href, this.rule.getUrl()));
+            }
+        } else {
+            // 目录第一页
+            urls.add(url);
         }
 
         List<Chapter> catalog = new ArrayList<>();
-        for (int i = start - 1; i < end && i < elements.size(); i++) {
-            Chapter build = Chapter.builder()
-                    .title(elements.get(i).text())
-                    .url(CrawlUtils.normalizeUrl(elements.get(i).attr("href"), this.rule.getUrl()))
-                    .chapterNo(i + 1)
-                    .build();
-            catalog.add(build);
+        int orderNumber = 1;
+
+        for (String s : urls) {
+            Document catalogPage = getConn(s, TIMEOUT_MILLS).get();
+            List<Element> elements = catalogPage.select(catalogRule.getResult());
+            if (offset != 0) {
+                if (offset > 0) elements = elements.subList(offset, elements.size());
+                if (offset < 0) elements = elements.subList(0, elements.size() + offset);
+            }
+
+            for (int i = start - 1; i < end && i < elements.size(); i++) {
+                Chapter build = Chapter.builder()
+                        .title(elements.get(i).text())
+                        .url(CrawlUtils.normalizeUrl(elements.get(i).attr("href"), this.rule.getUrl()))
+                        .chapterNo(orderNumber++)
+                        .build();
+                catalog.add(build);
+            }
         }
 
         return catalog;
