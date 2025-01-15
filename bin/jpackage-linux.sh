b81cfc4096c409f91b5e044a58f178ca83eeaa7e
freeok <51998152+freeok@users.noreply.github.com>
2025-01-15 17:55:18 +0800

    :hammer: 更新jpackage脚本

--- bin/jpackage-linux.sh
+++ bin/jpackage-linux.sh
@@ -1,28 +1,29 @@
-# 测试通过
-
-rm -rf dist
+project_path=$(
+  cd "$(dirname "$0")" || exit
+  cd ..
+  pwd
+)
+cd "$project_path" || exit
 
 mvn clean package "-Dmaven.test.skip=true"
-
 mkdir target/jpackage
 mv target/app-jar-with-dependencies.jar target/jpackage
 
+echo "jpackage version: $(jpackage --version)"
 # 在 linux 环境会转换为 .deb 文件
 # type=app-image 记得去掉--about-url
 jpackage \
 --name "SoNovel" \
 --input "target/jpackage" \
 --dest dist \
---icon assets/logo.ico \
---app-version 1.5.3 \
---copyright "Copyright (C) 2024 pcdd. All rights reserved." \
+--icon assets/logo-1.ico \
+--app-version 1.7.4 \
+--copyright "Copyright (C) 2025 SoNovel. All rights reserved." \
 --description "开源搜书神器" \
---vendor "github.com/freeok" \
---about-url "github.com/freeok/so-novel" \
+--vendor "FreeOK" \
 --main-jar "app-jar-with-dependencies.jar"
 
-cp config.ini dist/SoNovel
-rm dist/SoNovel/*.ico
+cp config.ini input/readme.txt dist/SoNovel
 
-echo "开始打包压缩"
+echo "开始压缩打包"
 tar czvf dist/SoNovel_linux.tar.gz dist/SoNovel
--- bin/jpackage-windows-dev.sh
+++ bin/jpackage-windows-dev.sh
@@ -1,12 +1,21 @@
-#
+# https://docs.oracle.com/en/java/javase/17/docs/specs/man/jpackage.html
+# 执行该脚本前前需注释 pom.xml > launch4j-maven-plugin
+
+export LANG=zh_CN.UTF-8
+
+project_path=$(
+  cd "$(dirname "$0")" || exit
+  cd ..
+  pwd
+)
+cd "$project_path" || exit
 
 rm -rf dist
-
 mvnd clean package "-Dmaven.test.skip=true"
-
 mkdir target/jpackage
 mv target/app-jar-with-dependencies.jar target/jpackage
 
+echo "jpackage version: $(jpackage --version)"
 # app-image 表示免安装版，部分参数不支持
 # 换行符 psh 为 `，cmd 为 ^，linux 为 \
 jpackage \
@@ -15,16 +24,12 @@ jpackage \
 --win-console \
 --input "target/jpackage" \
 --dest dist \
---icon assets/logo.ico \
---app-version 1.5.3 \
---copyright "Copyright (C) 2024 pcdd. All rights reserved." \
+--icon assets/logo-1.ico \
+--app-version 1.7.4 \
+--copyright "Copyright (C) 2025 SoNovel. All rights reserved." \
 --description "开源搜书神器" \
---vendor "github.com/freeok" \
+--vendor "FreeOK" \
 --main-jar "app-jar-with-dependencies.jar"
 
-cp config.ini dist/SoNovel
-rm dist/SoNovel/*.ico
-
-
-
-
+mkdir -p dist/SoNovel
+cp config.ini input/readme.txt dist/SoNovel
