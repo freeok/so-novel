7a077daedd4aede06e1354bf257c61e9a1b4ef47
freeok <51998152+freeok@users.noreply.github.com>
2025-01-23 04:00:46 +0800

    :recycle: 规范language属性命名

--- src/main/java/com/pcdd/sonovel/util/Language.java
+++ src/main/java/com/pcdd/sonovel/util/Language.java
@@ -9,8 +9,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Language {
 
-    public final String ZH_CN = "zh-CN";
-    public final String ZH_TW = "zh-TW";
-    public final String ZH_HANT = "zh-Hant";
+    public final String ZH_CN = "zh_CN";
+    public final String ZH_TW = "zh_TW";
+    public final String ZH_HANT = "zh_Hant";
 
 }
