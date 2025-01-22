8ecd41da957b84cceb3dee496c332f62d0cbad7b
freeok <51998152+freeok@users.noreply.github.com>
2025-01-23 03:33:01 +0800

    Add Language.java

---
+++ src/main/java/com/pcdd/sonovel/util/Language.java
@@ -0,0 +1,16 @@
+package com.pcdd.sonovel.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author pcdd
+ * Created at 2025/1/23
+ */
+@UtilityClass
+public class Language {
+
+    public final String ZH_CN = "zh-CN";
+    public final String ZH_TW = "zh-TW";
+    public final String ZH_HANT = "zh-Hant";
+
+}
