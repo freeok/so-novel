a1a83b1ac72edc2cf17fb56eb1c6ca87e5f1862e
freeok <51998152+freeok@users.noreply.github.com>
2025-01-29 21:38:34 +0800

    Add SourceUtils.java

---
+++ src/main/java/com/pcdd/sonovel/util/SourceUtils.java
@@ -0,0 +1,16 @@
+package com.pcdd.sonovel.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author pcdd
+ * Created at 2025/1/29
+ */
+@UtilityClass
+public class SourceUtils {
+
+    public int getSourceCount() {
+        return 6;
+    }
+
+}
