a28d4d4b97275d9fe330bc037ffa623b6f9f94b8
freeok <51998152+freeok@users.noreply.github.com>
2025-01-29 22:29:38 +0800

    :tada: Release v1.7.6

--- bin/jpackage-windows.sh
+++ bin/jpackage-windows.sh
@@ -18,7 +18,7 @@ jpackage \
 --input "nobug/jpackage" \
 --dest dist \
 --icon assets/logo-1.ico \
---app-version 1.7.5 \
+--app-version 1.7.6 \
 --copyright "Copyright (C) 2025 SoNovel. All rights reserved." \
 --description "开源搜书神器" \
 --vendor "FreeOK" \
