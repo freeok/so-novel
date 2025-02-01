db09d219ec1c76e61244c37b93e7a6f24342d20a
freeok <51998152+freeok@users.noreply.github.com>
2025-02-02 01:53:03 +0800

    :recycle: 优化源站搜索结果

--- src/main/java/com/pcdd/sonovel/handle/SearchResultsHandler.java
+++ src/main/java/com/pcdd/sonovel/handle/SearchResultsHandler.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class SearchResultsHandler {
 
     /**
-     * 对源站的搜索结果进行优化
+     * 优化源站的搜索结果
      */
     public List<SearchResult> handle(List<SearchResult> list) {
         // 按照作者分组
@@ -23,6 +23,9 @@ public class SearchResultsHandler {
 
         // 对每个组进行排序
         return authorGroups.values().stream()
+                // 根据每个作者对应的书籍数量进行降序排序
+                .sorted((entry1, entry2) -> Integer.compare(entry2.size(), entry1.size()))
+                // 每个作者的多个作品按书名排序，而只有一个作品的作者则不做排序
                 .map(group -> group.size() > 1
                         ? group.stream().sorted(Comparator.comparing(SearchResult::getBookName)).toList()
                         : group)
