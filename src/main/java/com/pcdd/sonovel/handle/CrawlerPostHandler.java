18442ecc3308f878fc87955efa5003baab54cfea
freeok <51998152+freeok@users.noreply.github.com>
2025-02-05 04:52:10 +0800

    :bug: 修复非全本下载时，生成的目录文件内容错误

--- src/main/java/com/pcdd/sonovel/handle/CrawlerPostHandler.java
+++ src/main/java/com/pcdd/sonovel/handle/CrawlerPostHandler.java
@@ -1,10 +1,12 @@
 package com.pcdd.sonovel.handle;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
 import com.pcdd.sonovel.model.AppConfig;
 import com.pcdd.sonovel.model.Book;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.io.File;
 
@@ -17,6 +19,7 @@ public class CrawlerPostHandler {
 
     private final AppConfig config;
 
+    @SneakyThrows
     public void handle(Book book, File saveDir) {
         String extName = config.getExtName();
         StringBuilder s = new StringBuilder(StrUtil.format("\n<== 《{}》（{}）下载完毕，", book.getBookName(), book.getAuthor()));
@@ -28,6 +31,13 @@ public class CrawlerPostHandler {
             s.append("正在生成 HTML 目录文件");
         }
         Console.log(s.append(" ..."));
+
+        // 等待文件系统更新索引
+        int attempts = 10;
+        while (FileUtil.isDirEmpty(saveDir) && attempts > 0) {
+            Thread.sleep(100);
+            attempts--;
+        }
 
         PostHandlerFactory.getHandler(extName, config).handle(book, saveDir);
     }
