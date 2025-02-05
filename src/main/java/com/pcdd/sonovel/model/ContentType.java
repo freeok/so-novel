3dc5b5d6a553f3c2ef406429102d0dd9071698df
freeok <51998152+freeok@users.noreply.github.com>
2025-02-05 12:14:14 +0800

    :sparkles: 新增CrawlUtils#selectAndInvokeJs

---
+++ src/main/java/com/pcdd/sonovel/model/ContentType.java
@@ -0,0 +1,22 @@
+package com.pcdd.sonovel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author pcdd
+ * Created at 2025/2/5
+ */
+@Getter
+@AllArgsConstructor
+public enum ContentType {
+
+    TEXT("text"),
+    HTML("html"),
+    ATTR_SRC("src"),
+    ATTR_HREF("href"),
+    ATTR_CONTENT("content");
+
+    private final String value;
+
+}
