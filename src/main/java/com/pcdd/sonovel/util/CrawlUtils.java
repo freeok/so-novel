5c3f14c7d65b5b53cee0ee141b78dbb2c76b99e6
freeok <51998152+freeok@users.noreply.github.com>
2025-02-06 07:15:52 +0800

    :recycle: CrawlUtils.java解耦

--- src/main/java/com/pcdd/sonovel/util/CrawlUtils.java
+++ src/main/java/com/pcdd/sonovel/util/CrawlUtils.java
@@ -1,23 +1,16 @@
 package com.pcdd.sonovel.util;
 
 import cn.hutool.core.lang.Validator;
-import cn.hutool.core.util.StrUtil;
 import cn.hutool.core.util.URLUtil;
 import cn.hutool.json.JSONUtil;
-import cn.hutool.script.ScriptUtil;
 import com.pcdd.sonovel.model.AppConfig;
-import com.pcdd.sonovel.model.ContentType;
 import lombok.experimental.UtilityClass;
 import org.jsoup.Connection;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.pcdd.sonovel.model.ContentType.*;
 
 
 /**
@@ -26,102 +19,6 @@ import static com.pcdd.sonovel.model.ContentType.*;
  */
 @UtilityClass
 public class CrawlUtils {
-
-    /**
-     * 使用查询条件选择元素
-     */
-    public Elements select(Element e, String query) {
-        // 分割查询条件以提取 XPath 或 CSS 查询
-        String actualQuery = StrUtil.subBefore(query, "##", false);
-        // 根据查询条件选择元素
-        if (actualQuery.startsWith("/")) {
-            return e.selectXpath(actualQuery);
-        }
-        return e.select(actualQuery);
-    }
-
-    /**
-     * 执行 JS 脚本并返回处理结果
-     */
-    public String invokeJs(String query, String input) {
-        if (StrUtil.isEmpty(query)) {
-            return input;
-        }
-        // 分割 JS 脚本查询条件，提取函数名
-        String[] split = query.split("##");
-        // 无 JS，返回原始输入
-        if (split.length == 1) {
-            return input;
-        }
-        return (String) ScriptUtil.invoke(split[1], "func", input);
-    }
-
-    /**
-     * 根据查询条件选择元素并执行可能的 JS 脚本
-     */
-    public String selectAndInvokeJs(Element e, String query, ContentType contentType) {
-        if (StrUtil.isEmpty(query)) {
-            return getContent(e, contentType); // 直接获取内容
-        }
-
-        // 分割查询条件
-        String[] split = query.split("##");
-        String actualQuery = split[0]; // 实际的选择器或 XPath
-
-        // 根据查询条件选择元素
-        Elements elements = select(e, actualQuery);
-
-        // 如果没有找到任何元素，返回空字符串
-        if (elements.isEmpty()) {
-            return "";
-        }
-
-        // 获取选中元素的内容
-        String result = getContents(elements, contentType);
-
-        // 如果查询条件包含 JS 执行部分，调用它
-        if (split.length == 2) {
-            return invokeJs(query, result);
-        }
-
-        return result;
-    }
-
-    public String getStrAndInvokeJs(Element e, String query, ContentType contentType) {
-        // 先获取元素的内容
-        String result = CrawlUtils.getContent(e, contentType);
-        // 如果查询条件包含 JS 执行部分，调用它
-        if (StrUtil.isNotEmpty(query)) {
-            result = CrawlUtils.invokeJs(query, result);
-        }
-        return result;
-    }
-
-    /**
-     * 获取元素的内容
-     */
-    public String getContent(Element el, ContentType contentType) {
-        return switch (contentType) {
-            case TEXT -> el.text();
-            case HTML -> el.html();
-            case ATTR_SRC -> el.attr(ATTR_SRC.getValue());
-            case ATTR_HREF -> el.attr(ATTR_HREF.getValue());
-            case ATTR_CONTENT -> el.attr(ATTR_CONTENT.getValue());
-        };
-    }
-
-    /**
-     * 获取元素的多个内容
-     */
-    private String getContents(Elements els, ContentType contentType) {
-        return switch (contentType) {
-            case TEXT -> els.text();
-            case HTML -> els.html();
-            case ATTR_SRC -> els.attr(ATTR_SRC.getValue());
-            case ATTR_HREF -> els.attr(ATTR_HREF.getValue());
-            case ATTR_CONTENT -> els.attr(ATTR_CONTENT.getValue());
-        };
-    }
 
     // 有的 href 是相对路径，需要拼接为完整路径
     public String normalizeUrl(String s, String host) {
