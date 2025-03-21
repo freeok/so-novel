102eae6d2fb898e7c11d62ca7cc0082226a45d66
freeok <51998152+freeok@users.noreply.github.com>
2025-03-22 05:14:27 +0800

    :sparkles: 书源5替换为：新天禧小说 https://github.com/freeok/so-novel/issues/112

--- src/test/java/com/pcdd/sonovel/BookSourceTest.java
+++ src/test/java/com/pcdd/sonovel/BookSourceTest.java
@@ -1,6 +1,7 @@
 /* 野兽主席 皇帝梦 修宪 爱新觉罗·近平 NMSL 维尼挂彩 习##卡##巴##卡 WHO的君王 “梦回大清” 人类命运共同体首席架构师 祈翠 维尼DISCO 塔西佗陷阱 习蛤 断崖式下跌 神明论 信女愿一生吃素 电脑世代586 冲塔 毛近平 世宗 中国拉清单工作领导小组组长 亲自脱贫 386系统驱动游戏 迈克尔·索伊 赵家家仆 SB包子 shuu##kinn##pei 白猪 当代秦二世 Cicada##3301##XD 习是春竹 修宪连任 中国爱非洲 习夶 晚共昏君 席近平 老歪脖子树 发皇帝梦 法习斯 习博士 崇祯 习太孑 维尼熊 包惠帝 总书记来我家 大锅瞻遗 悉包皮 第一书记王沪宁 末代皇帝 习梦死 习king 称帝修宪 习卒习 孢 智商不重要 惜包皮 连专家也为之叹服的电脑天才 细瓶颈 系包皮 亲自指挥 Heil##Xitler 慨撒大帝 Adolf##Xitler 维尼快乐组织（Winnie##Happy##Organization）简称WHO 重地之战 人民战争 習敗北 总书记来过我的家 第22条军规 岿然不动 不知名氏 儿童习典 胡志平 维尼连任 包皇 世界最强乞丐 百万雄师事件 金科律玉 包包侠 主席贺词 彭主席（习主席走了太太接班） 200吨 学包分子 瘟疫领主习近平 倒车帝 《生物安全法》 習敗敗 口罩外交 北红尾鸲 习猪下台 不强自息 我是一个足球迷 讨习檄文 习殡法 中国离岸隐匿资产管理工作领导小组组组长 “登机”（与登基同音） 近身平A 毛进平 小习微信 动物庄园 洗尽贫 毛魔转世 禁评封号 孤独的毒王 习近砰 习皇的新装 习侯 皇帝的新衣 尼哥 骑巨龙 亲自考察 习大犬 扛麦帝 洗包皮 粪坑先生 親自視頻 追思焦裕禄 禁评的理由 4中组长 习奥赛斯库 shit##hole##bing##fa 维尼带货 背书单 时间控制 人民领袖 习得梁家书卷 屎禁评 孙笑川 中国特色射秽煮疫制度 领袖习澡水 号屎洞 习猪席 维尼熊给跳跳虎黄牌 学包积极分子 包包大人 大海掀翻小池塘 习驴 动森 邪大大 习二胖 川普的朋友or敌人 崇祯帝 集金pay 包禁评 感恩教育 梁家河 核平全世界 当代秦始皇 刁人13 腊肉包 习王的田野上 大撒币 习公奭 喜包皮 习三点 习核心 戏包皮 习废帝 卡近菲 瘟疫降生习近平 集近闭 傻大头 維尼頌 韦来书记 集金币 叼斤干 习二世胡亥 指定接班人 糖尿维尼 维尼梗有害 武汉委托李克强 书单吟诵者 通商宽衣 大大品牌 大国造疫 墙内人 习言乱语 枪毙名单 习包子的Fa “皇帝梦” 中字头组长 口罩大会 坡涛汹涌 终身领导人 坟头蹦迪 Criminal##Xi##"Xitler"##Jinping 哲欣 xdd bingbang帝 北京中南海支部书记 nmslese##dream 习索里尼 習大佬 习猪头 庆丰帝的千秋梦 一天游泳一千米 任志强 截颈平 巴拿马文件 A##Big##Deal 九评 独彩者 习尔布特 坡涛汹涌 武当七侠的政治斗争 形式主义防疫 习二卒 刁二将 头号球迷 冰棒外交 shit侯 人尽皆知的体育迷 食腐虫 习进棺 习呆呆 学包率 退党 颐使气指 宰相 庆丰包·勃列日涅夫 “黄袍加身” 乳包文化 习张三丰 刁大犬 游泳一千米 裹嘎举吸钢闸门 刁人13 颐指气使 红二代 做N届的主席 习犬犬 大大招牌 维持会长 习语录 维尼写史 闹得欢 习后主 清单帝 大撒幣 法外狂徒Lucy（德国） 习贼 毛孙 撒币宝 习大乞 习习蛤蛤 习emperor 假.."~bang！卒 习像章 赛禁评 维尼大帝 捐麦子 叩谢习恩 灰习牛 公车上书 读稿机 疫情蔓延 任大炮 叩谢习恩 野蛮其体魄 习教父 包帝巡游 家业论 没规划的葬礼 翡翠娘娘 不同意的举手##没有##没有 平&强 习远凸 习匪帝 Mr##Shithole 倒车 包子病毒 足球外交 云视察 外交习语 西朝鲜人民自治岛 没有网络安全就没有国家安全 通商宽衣 万岁 御板泽民 习包皮 字共狗 XD 习奥塞斯库 习语 习厉王 习付 习卒 习式病毒 头上三尺有神明 一带一路 维尼史 国贼习近平 老街 习时代 中国首席造梦大师 䒒(tiáo)菦(qín)苹(píng) 动物森友会 花花公子 Voice##of##Pooh 维尼之声 彩色熊 战“疫”金句 习背书 习屎黄 习一尊 疯狂宇宙 禁评大帝 宽衣帝 习“甩锅” 赵王 狙击手待命 小学居士 初二圣君 役民五术 黄俄 习家军 皇帝亲临忠诚的武汉 近言 抗麦套装 包子金刚腿 万岁来武汉了 青年大学包 总统包子习 习梦思 普习金 粪坑兵法 喷粪帝 羽日帝 日子像蜜甜 习包子 沼气专家 包经 恩维尔·霍习 习流氓 错误执行者 习是春绿 阿道夫-习 吃赵弹 人民领袖 格萨尔 屎侯 央视姓党 红旗下的蛋 包子露宪 The##Xi##Factor（习近平因素） virus##king 亲自删评 习瘟猪 庆丰称帝 720事件 梦回大清 刁近猴 乳制品 不敢高声语 习下台 屎進瓶 习Core electron8964 清华毕业 耀眼黄红黄 大兔 皇帝亲临忠诚的武汉 习武帝 赵人13 影子末帝 彩色哥 读书单 阿平 习达姆 黑白翠 习大 习匪 乳透社##小反旗 氼兲嫑炛 朝令夕改 每日祈翠 基拉大和习近平 习二蛋 普近 腊肉馅儿包子 赵付 维稳警察（O） 习孢子 扛麦郎 疯狂宇宙大帝 袁世凯第二 习尿瓶 刁槑夶尛 大撒币 富平学霸刁斤干 习二坐船 倒车斯基 习歪嘴 人均接近八千万 Chairman##Xi 超邓赶毛 习包子 习老八 国王 厕所革命 毛二习 支那精神野爹 猪禁评 那翠化 相信有神论的物理学博士 民进党 习思想 昆明湖涨潮 亲自指挥疫情 拉清单 九大诉求 阳台上的哨兵 十一郎习近平 共哀宗 V尼哥 刁近乎 支那毒王 习正日 共哀宗 秦城监狱 64岁 吸包皮 细颈瓶 总加速师 Corona##Xi（习病毒） 万岁来武汉 该违禁词汇由 GitCode 官方生成，仅用于测试，请勿传播！ 隔离”的金葫芦 戏博士 毛二 夶 包大人 习梦撕 习总 视频看望 平话 半羽习近平 狙击手待命 动物之森 戴口赵 ChinaXi 贬词包用 灭共助推器 维尼·屎(Winnie##the##Poo) 甩锅侠 武统时代 恐惊天上人 shit禁评 嘻包皮 习近平光辉形象 刁后主 习兵法 无限连任 “习泽东”和“洪宪”（袁世凯的帝号） 吸精瓶 习特勒 庆丰废物 习皇帝 太祖兔 毛二世 嬉包皮 满脸喷粪 习澳塞斯库 专治各种不服 刁远山 CoronaXi 野兽先辈 中国离岸隐匿资产管理工作领导小组组组长 中国的新皇帝 梦帝 裸宽包子劾心 墨索里习 添宪宝宝 鞋哥 习禁评 祈翠語言包 习狍 匪酋习维尼 平平 官僚主义 蔡英文最强助选 疯狂宇宙包 vop 习病法 近日成 玉习大帝 十日文革 席子兵法 东瘟疫之地统治者暨全境守护者 糟糕皇帝 仲夏夜之中国梦 赵弹 习总输记 战疫国 五毛主席 文明其精神 爱新觉罗近平 独裁者 新影帝 送礼平 当皇帝的小丑 親自蒞臨 包子宪法 现任匪首 两会期间+隐瞒 “复辟” XDD 东方project 这个年很静 习二二（二零二零年五月二十二日开二会） “登基” 你是反贼吗 习大林 毛装 尊蛤讨包 二百五大帝 习三拍 最后领导人 包子兵法 冤大头 习大帝 蹄防 帝皇頌 刁斤二 刁二婚 nmslman（习主席） 20000T麦子 小学博士 袭包皮 宽衣撸绣单肩扛麦光腚拉碾歪脖撒币小学博士帝 次级乳包 冠子兵法 习包子 “无罩”现身 纳翠党 习和谐 劝进 习条英机 恩重如删 号尿壶公 维尼警察（X） 野心习 习躲躲 习大郎 狙击手布阵 亲自封号 习宽衣 天安门合法继承人 梁家河贵族 10号跳跳虎被维尼拉清单 主席+终身制 2020全面小康 习低能儿 刁斤山 */
 package com.pcdd.sonovel;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.Console;
 import cn.hutool.json.JSONUtil;
 import cn.hutool.log.dialect.console.ConsoleLog;
@@ -49,6 +50,7 @@ class BookSourceTest {
             "2, https://www.shuhaige.net/199178/, https://www.shuhaige.net/199178/86580492.html",
             "3, http://www.mcmssc.la/145_145199/, http://www.mcmssc.la/145_145199/57831284.html",
             "4, http://www.99xs.info/tag/129_129843/, http://www.99xs.info/tag/129_129843/47783670.html",
+            "5, https://www.tianxibook.com/book/94376180/, https://www.tianxibook.com/xiaoshuo/94376180/174210497.html",
             "8, https://www.dxmwx.org/book/56441.html, https://www.dxmwx.org/read/56441_49483830.html",
             "9, https://www.369book.cc/book/38894/, https://www.369book.cc/read/38894/40253.html",
             "10, https://cn.ttkan.co/novel/chapters/tunshixingkongzhiwuzuchuanshuo-dugujiujie, https://cn.wa01.com/novel/pagea/tunshixingkongzhiwuzuchuanshuo-dugujiujie_367.html",
@@ -57,6 +59,7 @@ class BookSourceTest {
             "13, https://www.deqixs.com/xiaoshuo/323/, https://www.deqixs.com/xiaoshuo/323/369060.html",
             "14, https://www.xbqg06.com/1582/, https://www.xbqg06.com/1582/4559.html",
             "15, https://www.luegeng.com/book186856/, https://www.luegeng.com/book186856/2448008/",
+            // "16, https://www.96dushu.com/book/433050/, https://www.96dushu.com/book/433050/145637288.html",
     })
     void testDirectSources(int sourceId, String bookUrl, String chapterUrl) {
         this.bookUrl = bookUrl;
@@ -64,7 +67,7 @@ class BookSourceTest {
 
         config.setSourceId(sourceId);
 
-        searchParse("唐家三少");
+        searchParse("辰东");
         bookParse();
         chapterParse();
         tocParse();
@@ -73,20 +76,19 @@ class BookSourceTest {
     @DisplayName("测试代理书源")
     @ParameterizedTest
     @CsvSource({
-            "5, https://69shux.co/book/54619.html, https://69shux.co/txt/54619/23367169.html",
             "6, https://quanben5.com/n/xinghedadi/, https://quanben5.com/n/xinghedadi/29882.html",
-            "7, https://69shuba.cx/book/58911.htm, https://69shuba.cx/txt/58911/38355484",
+            "7, https://www.69yuedu.net/article/emtzibcrtd.html, https://www.69yuedu.net/r/emtzibcrtd/ykhkuehnfeskigud.html",
     })
     void testProxySources(int sourceId, String bookUrl, String chapterUrl) {
         this.bookUrl = bookUrl;
         this.chapterUrl = chapterUrl;
 
         config.setSourceId(sourceId);
-        config.setProxyEnabled(0);
+        config.setProxyEnabled(1);
         config.setProxyHost("127.0.0.1");
         config.setProxyPort(7890);
 
-        searchParse("唐家三少");
+        searchParse("斗罗大陆");
         bookParse();
         chapterParse();
         tocParse();
@@ -99,6 +101,9 @@ class BookSourceTest {
             list = new SearchResultParser6(config).parse(keyword);
         } else {
             list = new SearchResultParser(config).parse(keyword);
+        }
+        if (CollUtil.isEmpty(list)) {
+            Console.log("\"{}\"搜索结果为空", keyword);
         }
         new SearchResultParser(config).printSearchResult(list);
         Console.log("{} END searchParse {}\n", DIVIDER, DIVIDER);
@@ -115,7 +120,6 @@ class BookSourceTest {
         Console.log("\n{} START tocParse {}", DIVIDER, DIVIDER);
         TocParser tocParser = new TocParser(config);
         List<Chapter> parse = tocParser.parse(bookUrl);
-        // tocParser.shutdown();
         parse.forEach(System.out::println);
         Console.log("{} END tocParse {}\n", DIVIDER, DIVIDER);
     }
