ae01fca3e249b8f08a1d82cd642e5fdbfae47607
freeok <51998152+freeok@users.noreply.github.com>
2025-04-05 02:35:45 +0800

    :bug: 修复正文内容包含<br>

--- src/main/java/com/pcdd/sonovel/core/ChapterFormatter.java
+++ src/main/java/com/pcdd/sonovel/core/ChapterFormatter.java
@@ -20,18 +20,18 @@ public class ChapterFormatter {
     public String format(String content) {
         Rule.Chapter r = new Source(config).rule.getChapter();
 
-        // 标签闭合
+        // <p>段落</p>
         if (r.isParagraphTagClosed()) {
-            // 非 <p> 闭合标签，替换为 <p>
+            // 非 <p> 闭合标签（例如 <span>段落</span>）替换为 <p>
             return content.replaceAll("<(?!p\\b)([^>]+)>(.*?)</\\1>", "<p>$2</p>");
         }
         // 标签不闭合，用某个标签分隔，例如：段落1<br><br>段落2
         String tag = r.getParagraphTag();
         StringBuilder sb = new StringBuilder();
 
-        for (String s : content.split(tag)) {
-            if (!s.isBlank()) {
-                sb.append("<p>").append(s).append("</p>");
+        for (String line : content.split(tag)) {
+            if (!line.isBlank()) {
+                sb.append("<p>").append(line).append("</p>");
             }
         }
 
