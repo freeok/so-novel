22b3423d6b76a215658ec8ab362aab0f7a4cfa8a
freeok <51998152+freeok@users.noreply.github.com>
2025-04-10 18:04:58 +0800

    :recycle: 更新书源质量测试

--- src/test/java/com/pcdd/sonovel/BookSourceQualityTest.java
+++ src/test/java/com/pcdd/sonovel/BookSourceQualityTest.java
@@ -1,10 +1,11 @@
 /* 瘟疫降生习近平 粪坑兵法 《生物安全法》 包包侠 九大诉求 羽日帝 “黄袍加身” 刁斤山 修宪 国贼习近平 口罩外交 A##Big##Deal 总统包子习 撒币宝 习奥塞斯库 习时代 视频看望 习后主 野兽主席 半羽习近平 小学博士 胡志平 习博士 总加速师 次级乳包 学包率 人均接近八千万 维尼警察（X） 倒车 亲自删评 习背书 不知名氏 庆丰帝的千秋梦 富平学霸刁斤干 吃赵弹 嘻包皮 习废帝 新影帝 “登机”（与登基同音） 末代皇帝 断崖式下跌 截颈平 习“甩锅” 禁评的理由 维尼写史 退党 动物庄园 shit禁评 习厉王 shuu##kinn##pei 错误执行者 形式主义防疫 亲自脱贫 儿童习典 假.."~bang！卒 格萨尔 包惠帝 习皇帝 平话 习包子 疯狂宇宙包 恩重如删 不强自息 共哀宗 阿道夫-习 习王的田野上 武汉委托李克强 太祖兔 屎侯 习总输记 枪毙名单 习武帝 维尼连任 万岁来武汉了 亲自封号 赵付 刁近猴 倒车帝 中国离岸隐匿资产管理工作领导小组组组长 野兽先辈 包大人 乳透社##小反旗 最后领导人 红二代 包子病毒 习大犬 夶 邪大大 4中组长 习近平光辉形象 皇帝梦 当代秦二世 維尼頌 第22条军规 当皇帝的小丑 青年大学包 Heil##Xitler 哲欣 近日成 动森 大大招牌 习屎黄 毛二习 冤大头 习二蛋 习包皮 央视姓党 岿然不动 十日文革 官僚主义 骑巨龙 V尼哥 大锅瞻遗 北京中南海支部书记 一天游泳一千米 人尽皆知的体育迷 东瘟疫之地统治者暨全境守护者 近身平A 习包子 字共狗 独彩者 颐指气使 习大 白猪 送礼平 维尼带货 XDD 毛近平 通商宽衣 不同意的举手##没有##没有 维尼熊 习大乞 赛禁评 恩维尔·霍习 坡涛汹涌 墙内人 任大炮 爱新觉罗·近平 梦回大清 系包皮 维尼挂彩 读书单 习猪席 386系统驱动游戏 感恩教育 习正日 亲自指挥疫情 vop 狙击手布阵 彭主席（习主席走了太太接班） 民进党 bingbang帝 习低能儿 裹嘎举吸钢闸门 读稿机 习二世胡亥 CoronaXi 动物之森 习近砰 我是一个足球迷 抗麦套装 初二圣君 习远凸 书单吟诵者 甩锅侠 公车上书 习总 氼兲嫑炛 袭包皮 习家军 韦来书记 匪酋习维尼 每日祈翠 川普的朋友or敌人 宰相 习梦死 背书单 习殡法 糖尿维尼 毛孙 习大郎 拉清单 shit##hole##bing##fa 迈克尔·索伊 疯狂宇宙大帝 万岁来武汉 XD 人民领袖 武当七侠的政治斗争 维尼梗有害 大兔 发皇帝梦 刁斤二 习孢子 Cicada##3301##XD 恐惊天上人 东方project 狙击手待命 Voice##of##Pooh 粪坑先生 倒车斯基 信女愿一生吃素 习大帝 梦帝 刁二婚 習敗敗 大大品牌 隔离”的金葫芦 口罩大会 习是春竹 宽衣撸绣单肩扛麦光腚拉碾歪脖撒币小学博士帝 红旗下的蛋 包皇 习猪头 中国特色射秽煮疫制度 耀眼黄红黄 十一郎习近平 世界最强乞丐 糟糕皇帝 食腐虫 习emperor 闹得欢 戏包皮 任志强 梁家河贵族 普近 毛魔转世 塔西佗陷阱 维尼快乐组织（Winnie##Happy##Organization）简称WHO 战“疫”金句 习条英机 崇祯帝 习侯 洗尽贫 习得梁家书卷 Adolf##Xitler 200吨 习习蛤蛤 彩色哥 习躲躲 彩色熊 习三点 朝令夕改 禁评封号 亲自指挥 “习泽东”和“洪宪”（袁世凯的帝号） 修宪连任 连专家也为之叹服的电脑天才 支那毒王 习瘟猪 老街 习贼 仲夏夜之中国梦 赵弹 黑白翠 禁评大帝 包经 习式病毒 刁二将 毛二 西朝鲜人民自治岛 号尿壶公 普习金 百万雄师事件 神明论 习付 爱新觉罗近平 赵家家仆 屎進瓶 疯狂宇宙 坡涛汹涌 习king 习皇的新装 昆明湖涨潮 学包积极分子 刁近乎 颐使气指 疫情蔓延 指定接班人 超邓赶毛 习梦思 细瓶颈 习禁评 人民战争 习兵法 惜包皮 “无罩”现身 头号球迷 大撒幣 The##Xi##Factor（习近平因素） 习进棺 人类命运共同体首席架构师 智商不重要 蹄防 习二卒 20000T麦子 中国首席造梦大师 学包分子 平&强 老歪脖子树 親自視頻 “复辟” 习Core 动物森友会 小学居士 ChinaXi 足球外交 外交习语 核平全世界 亲自考察 集金pay 赵王 袁世凯第二 傻大头 习像章 悉包皮 维尼大帝 乳制品 包子露宪 习思想 喜包皮 SB包子 腊肉包 习蛤 刁人13 习和谐 “皇帝梦” 相信有神论的物理学博士 毛二世 狙击手待命 习卒习 习奥赛斯库 法习斯 近言 清华毕业 刁远山 支那精神野爹 猪禁评 孙笑川 刁大犬 大撒币 习是春绿 灭共助推器 现任匪首 习包子 习梦撕 黄俄 中字头组长 习语录 庆丰称帝 尼哥 中国离岸隐匿资产管理工作领导小组组组长 习病法 大国造疫 一带一路 添宪宝宝 毛进平 包子宪法 帝皇頌 主席贺词 维尼熊给跳跳虎黄牌 习公奭 冲塔 习特勒 清单帝 嬉包皮 通商宽衣 重地之战 秦城监狱 习大林 讨习檄文 Mr##Shithole 吸包皮 习尿瓶 大海掀翻小池塘 习索里尼 戴口赵 冰棒外交 10号跳跳虎被维尼拉清单 武统时代 习宽衣 影子末帝 劝进 役民五术 家业论 習敗北 追思焦裕禄 屎禁评 该违禁词汇由 GitCode 官方生成，仅用于测试，请勿传播！ 维尼DISCO 共哀宗 没规划的葬礼 维尼史 宽衣帝 习包子的Fa 习下台 shit侯 世宗 习太孑 洗包皮 野蛮其体魄 战疫国 卡近菲 electron8964 习狍 席近平 习卒 皇帝亲临忠诚的武汉 集近闭 小习微信 文明其精神 nmslese##dream 翡翠娘娘 親自蒞臨 野心习 席子兵法 Criminal##Xi##"Xitler"##Jinping 赵人13 习流氓 称帝修宪 中国的新皇帝 第一书记王沪宁 平平 扛麦郎 中国爱非洲 包帝巡游 独裁者 祈翠 WHO的君王 习呆呆 习歪嘴 习犬犬 2020全面小康 满脸喷粪 xdd 习老八 刁槑夶尛 包子金刚腿 叩谢习恩 纳翠党 戏博士 乳包文化 细颈瓶 主席+终身制 腊肉馅儿包子 孢 庆丰包·勃列日涅夫 无限连任 维稳警察（O） 毛装 头上三尺有神明 沼气专家 习教父 集金币 习匪 梁家河 中国拉清单工作领导小组组长 灰习牛 人民领袖 花花公子 習大佬 习尔布特 习##卡##巴##卡 坟头蹦迪 720事件 厕所革命 两会期间+隐瞒 玉习大帝 捐麦子 习二二（二零二零年五月二十二日开二会） 基拉大和习近平 领袖习澡水 北红尾鸲 包禁评 崇祯 国王 金科律玉 不敢高声语 巴拿马文件 御板泽民 蔡英文最强助选 天安门合法继承人 总书记来过我的家 习一尊 那翠化 号屎洞 云视察 扛麦帝 䒒(tiáo)菦(qín)苹(píng) 习张三丰 总书记来我家 习澳塞斯库 电脑世代586 吸精瓶 “登基” 这个年很静 喷粪帝 64岁 习达姆 法外狂徒Lucy（德国） 九评 习猪下台 你是反贼吗 专治各种不服 习匪帝 墨索里习 二百五大帝 慨撒大帝 习核心 NMSL 叼斤干 习二胖 冠子兵法 晚共昏君 习夶 尊蛤讨包 时间控制 庆丰废物 维尼·屎(Winnie##the##Poo) 万岁 叩谢习恩 贬词包用 孤独的毒王 鞋哥 瘟疫领主习近平 终身领导人 习三拍 游泳一千米 阿平 裸宽包子劾心 刁人13 五毛主席 维持会长 习言乱语 刁后主 维尼之声 习驴 Corona##Xi（习病毒） 大撒币 皇帝亲临忠诚的武汉 阳台上的哨兵 皇帝的新衣 习二坐船 “梦回大清” 没有网络安全就没有国家安全 习语 祈翠語言包 包包大人 Chairman##Xi nmslman（习主席） 包子兵法 做N届的主席 日子像蜜甜 virus##king 当代秦始皇 */
 package com.pcdd.sonovel;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.lang.Console;
-import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.core.util.URLUtil;
 import cn.hutool.http.Header;
@@ -18,10 +19,10 @@ import com.pcdd.sonovel.model.SearchResult;
 import com.pcdd.sonovel.parse.SearchParser;
 import com.pcdd.sonovel.parse.SearchParser6;
 import com.pcdd.sonovel.util.ConfigUtils;
+import com.pcdd.sonovel.util.JsoupUtils;
 import com.pcdd.sonovel.util.RandomUA;
 import com.pcdd.sonovel.util.SourceUtils;
 import lombok.Data;
-import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -30,6 +31,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -47,11 +49,13 @@ class BookSourceQualityTest {
 
     static final AppConfig config = ConfigUtils.config();
     static final Map<String, List<Book>> ranks = new ConcurrentHashMap<>();
-    // 0 < TOP_NUM <= 20
-    public static final int TOP_NUM = 20;
-    public static final String RE_SKIP_IDS = "6|9|10";
+    // 测试排行榜前几名 (0,20]
+    static final int TOP_NUM = 20;
+    // 1：封IP，6：老书，12、15：限流，15：523错误
+    static final String RE_SKIP_IDS = "1|6|12|15|16";
 
     static {
+        JsoupUtils.trustAllSSL();
         ConsoleLog.setLevel(Level.OFF);
         config.setLanguage("zh_CN");
     }
@@ -62,15 +66,19 @@ class BookSourceQualityTest {
         }
     }
 
-    @SneakyThrows
     List<Book> getQiDianRanks(String rankUrl) {
         Console.log("getQiDianRanks: {}", rankUrl);
         List<Book> rank = new ArrayList<>();
-        Document document = Jsoup.connect(rankUrl)
-                .timeout(3000)
-                .header(Header.USER_AGENT.getValue(), RandomUA.generate())
-                .header(Header.COOKIE.getValue(), "w_tsfp=ltvuV0MF2utBvS0Q6qPpnE2sFzsidD04h0wpEaR0f5thQLErU5mG2IZyuMn2NHDf6sxnvd7DsZoyJTLYCJI3dwMSRpqReokRhQ/ElYgnjtxAVBI1QJzYWAJJJLly7DdAf3hCNxS00jA8eIUd379yilkMsyN1zap3TO14fstJ019E6KDQmI5uDW3HlFWQRzaLbjcMcuqPr6g18L5a5TjetFupeV8iA+sXhU3B3HlKWC4gskCyIuAJNBmlI5j5SqA=")
-                .get();
+        Document document = null;
+        try {
+            document = Jsoup.connect(rankUrl)
+                    .timeout(3000)
+                    .header(Header.USER_AGENT.getValue(), RandomUA.generate())
+                    .header(Header.COOKIE.getValue(), "w_tsfp=ltvuV0MF2utBvS0Q6qPpnE2sFzsidD04h0wpEaR0f5thQLErU5mG2IZyuMn2NHDf6sxnvd7DsZoyJTLYCJI3dwMSRpqReokRhQ/ElYgnjtxAVBI1QJzYWAJJJLly7DdAf3hCNxS00jA8eIUd379yilkMsyN1zap3TO14fstJ019E6KDQmI5uDW3HlFWQRzaLbjcMcuqPr6g18L5a5TjetFupeV8iA+sXhU3B3HlKWC4gskCyIuAJNBmlI5j5SqA=")
+                    .get();
+        } catch (IOException e) {
+            Console.error(e);
+        }
 
         Elements elements = document.select("#book-img-text > ul > li");
         // 取前 N 名
@@ -149,14 +157,15 @@ class BookSourceQualityTest {
         }
     }
 
-    @SneakyThrows
     List<SourceQuality> getSourceQualityList(int id, Map.Entry<String, String> rank) {
         int foundCount = 0;
         int notFoundCount = 0;
         List<SourceQuality> list = new ArrayList<>();
         Rule rule = new Source(id).rule;
 
-        Console.log("<== {}，开始测试书源质量：书源 {} {} ({})", rank.getKey(), rule.getId(), rule.getUrl(), rule.getName());
+        String divider = "=".repeat(30);
+        Console.log("{} 测试书源质量 {} | 书源 {} {} ({}) {}",
+                divider, rank.getKey(), rule.getId(), rule.getUrl(), rule.getName(), divider);
         config.setSourceId(id);
         // 需要代理的书源
         config.setProxyEnabled(rule.isNeedProxy() ? 1 : 0);
@@ -195,9 +204,15 @@ class BookSourceQualityTest {
             }
             sq.setFound(found);
             list.add(sq);
-            int randomInt = RandomUtil.randomInt(500, 1000);
+
+            // 针对搜索限流书源的处理
+            /* int randomInt = RandomUtil.randomInt(200, 500);
             Console.log("搜索间隔 {} ms", randomInt);
-            Thread.sleep(randomInt);
+            try {
+                Thread.sleep(randomInt);
+            } catch (InterruptedException e) {
+                Console.error(e);
+            } */
         }
         Console.log("书源 {} ({})，{}已找到 {} 本，未找到 {} 本\n\n",
                 rule.getId(), rule.getUrl(), rank.getKey(), foundCount, notFoundCount);
@@ -205,8 +220,12 @@ class BookSourceQualityTest {
         return list;
     }
 
-    @SneakyThrows
     void generateMarkdown(String title, Map<Integer, List<SourceQuality>> map) {
+        if (MapUtil.isEmpty(map)) {
+            Console.error("{} sourceQualityListMap 为空", title);
+            return;
+        }
+
         String fileName = "qidian_rank/%s.md".formatted(title);
         Console.log("<== generateMarkdown: {}", fileName);
         // 表头
@@ -220,19 +239,20 @@ class BookSourceQualityTest {
         }
 
         StringBuilder md = new StringBuilder();
-        // # 起点xx榜前 20 (yyyy-MM-dd)
-        md.append(StrUtil.format("{}前 {} ({})\n",
+        // # 起点xx榜前 TOP_NUM (yyyy-MM-dd)
+        md.append(StrUtil.format("{}前 {} 名 ({})\n",
                 "# " + title,
-                map.get(1).size(),
+                TOP_NUM,
                 DateTime.now().toString(DatePattern.NORM_DATE_PATTERN)));
         md.append(StrUtil.format("| 排名 | 书名 | 作者 {} 起点链接 |\n", sourceNameCol));
         md.append(StrUtil.format("| ---- | ---- | ---- {} ---- |\n", dividerCol));
 
-        List<SourceQuality> list = map.get(1);
+        // 获取 map 任意非空元素的 value
+        List<SourceQuality> list = CollUtil.getFirst(CollUtil.filter(map.values(), Objects::nonNull));
         for (int i = 0; i < list.size(); i++) {
             SourceQuality o = list.get(i);
-
             StringBuilder foundBuilder = new StringBuilder();
+
             for (List<SourceQuality> item : map.values()) {
                 SourceQuality sq = item.get(i);
                 foundBuilder.append(StrUtil.format("{} |", sq.getFound() ? "✅" : "❌"));
@@ -246,12 +266,16 @@ class BookSourceQualityTest {
                     o.getQiDianUrl()));
         }
 
-        File file = new File(fileName);
-        file.getParentFile().mkdirs();
-        Writer writer = new FileWriter(file);
-        writer.write(md.toString());
-        writer.flush();
-        writer.close();
+        try {
+            File file = new File(fileName);
+            file.getParentFile().mkdirs();
+            Writer writer = new FileWriter(file);
+            writer.write(md.toString());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            Console.error(e);
+        }
     }
 
 }
