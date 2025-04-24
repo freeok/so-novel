8f664682c5dae3aab65765f4ded9e125e7c401fd
freeok <51998152+freeok@users.noreply.github.com>
2025-04-25 04:56:53 +0800

    :sparkles: 新增书源：阅读库 https://github.com/freeok/so-novel/issues/155

--- src/test/java/com/pcdd/sonovel/source/BookSourceTest.java
+++ src/test/java/com/pcdd/sonovel/source/BookSourceTest.java
@@ -2,9 +2,11 @@ package com.pcdd.sonovel.source;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.RuntimeUtil;
 import cn.hutool.json.JSONUtil;
 import cn.hutool.log.dialect.console.ConsoleLog;
 import cn.hutool.log.level.Level;
+import cn.hutool.system.SystemUtil;
 import com.pcdd.sonovel.convert.ChapterConverter;
 import com.pcdd.sonovel.convert.ChineseConverter;
 import com.pcdd.sonovel.core.Source;
@@ -15,12 +17,16 @@ import com.pcdd.sonovel.model.SearchResult;
 import com.pcdd.sonovel.parse.*;
 import com.pcdd.sonovel.util.ConfigUtils;
 import com.pcdd.sonovel.util.JsoupUtils;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author pcdd
@@ -34,6 +40,7 @@ class BookSourceTest {
     public static final String DIVIDER = "=".repeat(50);
     private String bookUrl;
     private String chapterUrl;
+    private List<Chapter> chapters;
 
     static {
         JsoupUtils.trustAllSSL();
@@ -51,7 +58,7 @@ class BookSourceTest {
             "2, https://www.shuhaige.net/199178/",
             "3, http://www.mcxs.info/145_145199/",
             "4, http://www.99xs.info/tag/129_129843/",
-            "5, https://www.tianxibook.com/book/94376180/",
+            "5, https://www.tianxibook.com/book/66120/",
             "8, https://www.dxmwx.org/book/56441.html",
             "9, https://www.22biqu.com/biqu79148/",
             "10, http://www.xbiquzw.net/10_10233/",
@@ -61,15 +68,18 @@ class BookSourceTest {
             "15, https://www.96dushu.com/book/344921/",
             "17, http://www.81zwwww.com/90_90170/",
             "18, http://www.ujxsw.net/book/107612/",
+            "18, http://www.ujxsw.net/book/107612/",
+            "19, http://www.yeudusk.com/book/1322535/",
     })
     void testDirectSources(int sourceId, String bookUrl) {
         this.bookUrl = bookUrl;
         config.setSourceId(sourceId);
 
-        searchParse("辰东");
+        searchParse("zhttty");
         bookParse();
         tocParse();
         chapterParse();
+        // chapterBatchParse(1, 850);
     }
 
     @DisplayName("测试代理书源")
@@ -84,7 +94,7 @@ class BookSourceTest {
         this.bookUrl = bookUrl;
         config.setSourceId(sourceId);
 
-        searchParse("辰东");
+        searchParse("zhttty");
         bookParse();
         tocParse();
         chapterParse();
@@ -119,6 +129,7 @@ class BookSourceTest {
         Console.log("\n{} START tocParse {}", DIVIDER, DIVIDER);
         TocParser tocParser = new TocParser(config);
         List<Chapter> toc = tocParser.parse(bookUrl);
+        chapters = toc;
         toc.forEach(System.out::println);
         if (CollUtil.isNotEmpty(toc)) {
             // 测试目录首章
@@ -138,6 +149,29 @@ class BookSourceTest {
         ChineseConverter.convert(afterFiltration, source.rule.getLanguage(), config.getLanguage());
         Console.log(afterFiltration.getContent());
         Console.log("{} END chapterParse {}\n", DIVIDER, DIVIDER);
+    }
+
+    @SneakyThrows
+    public void chapterBatchParse(int start, int end) {
+        Console.log("\n{} START chapterBatchParse {}", DIVIDER, DIVIDER);
+        ExecutorService threadPool = Executors.newFixedThreadPool(RuntimeUtil.getProcessorCount());
+        CountDownLatch latch = new CountDownLatch(end - start);
+        Source source = new Source(config.getSourceId());
+
+        for (Chapter chapter : CollUtil.sub(chapters, start, end)) {
+            threadPool.execute(() -> {
+                Chapter o = Chapter.builder().url(chapter.getUrl()).build();
+                Chapter beforeFiltration = new ChapterParser(config).parse(o);
+                Chapter afterFiltration = new ChapterConverter(config).convert(beforeFiltration);
+                ChineseConverter.convert(afterFiltration, source.rule.getLanguage(), config.getLanguage());
+                Console.log("✅ " + o.getTitle());
+                latch.countDown();
+            });
+        }
+
+        latch.await();
+        threadPool.shutdown();
+        Console.log("{} END chapterBatchParse {}\n", DIVIDER, DIVIDER);
     }
 
 }
