d1e7d578963ab9d5bef2659e03dcc16a00d0a8aa
freeok <51998152+freeok@users.noreply.github.com>
2025-04-30 21:03:24 +0800

    :recycle: 优化Crawler.java、ChapterParser.java

--- src/main/java/com/pcdd/sonovel/parse/ChapterParser.java
+++ src/main/java/com/pcdd/sonovel/parse/ChapterParser.java
@@ -1,5 +1,6 @@
 package com.pcdd.sonovel.parse;
 
+import cn.hutool.core.lang.Assert;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
@@ -7,6 +8,7 @@ import com.pcdd.sonovel.convert.ChapterConverter;
 import com.pcdd.sonovel.convert.ChineseConverter;
 import com.pcdd.sonovel.core.Source;
 import com.pcdd.sonovel.model.*;
+import com.pcdd.sonovel.util.BookContext;
 import com.pcdd.sonovel.util.CrawlUtils;
 import com.pcdd.sonovel.util.JsoupUtils;
 import lombok.SneakyThrows;
@@ -48,34 +50,42 @@ public class ChapterParser extends Source {
         }
 
         chapter.setTitle(JsoupUtils.selectAndInvokeJs(document, this.rule.getChapter().getTitle()));
-        chapter.setContent(crawl(chapter.getUrl(), RandomUtil.randomInt(100, 200)));
+        chapter.setContent(fetchContent(chapter.getUrl(), RandomUtil.randomInt(100, 200)));
+
         return chapter;
     }
 
-    public Chapter parse(Chapter chapter, CountDownLatch latch, SearchResult sr) {
+    public Chapter parse(Chapter chapter, CountDownLatch latch) {
         try {
             long interval = CrawlUtils.randomInterval(config);
             Console.log("<== 正在下载: 【{}】 间隔 {} ms", chapter.getTitle(), interval);
-            // ExceptionUtils.randomThrow();
-            chapter.setContent(crawl(chapter.getUrl(), interval));
+
+            String content = fetchContent(chapter.getUrl(), interval);
+            Assert.notEmpty(content, "正文内容为空");
+            chapter.setContent(content);
+
             // 确保简繁互转最后调用
             return ChineseConverter.convert(chapterConverter.convert(chapter), this.rule.getLanguage(), config.getLanguage());
 
         } catch (Exception e) {
-            Chapter retryChapter = retry(chapter, sr);
+            Chapter retryChapter = retry(chapter, e.getMessage());
             return retryChapter == null ? null : ChineseConverter.convert(retryChapter, this.rule.getLanguage(), config.getLanguage());
         } finally {
             latch.countDown();
         }
     }
 
-    private Chapter retry(Chapter chapter, SearchResult sr) {
+    private Chapter retry(Chapter chapter, String errMsg) {
         for (int attempt = 1; attempt <= config.getMaxRetryAttempts(); attempt++) {
             try {
                 long interval = CrawlUtils.randomInterval(config, true);
-                Console.log(render("<== 章节下载失败，正在重试: 【{}】 重试次数: {}/{} 重试间隔: {} ms", "red"),
-                        chapter.getTitle(), attempt, config.getMaxRetryAttempts(), interval);
-                chapter.setContent(crawl(chapter.getUrl(), interval));
+                Console.log(render("<== : 章节【{}】下载失败，正在重试。重试次数: {}/{} 重试间隔: {} ms 原因: {}", "red"),
+                        chapter.getTitle(), attempt, config.getMaxRetryAttempts(), interval, errMsg);
+
+                String content = fetchContent(chapter.getUrl(), interval);
+                Assert.notEmpty(content, "正文内容为空");
+                chapter.setContent(content);
+
                 Console.log(render("<== 重试成功: 【{}】", "green"), chapter.getTitle());
                 return chapterConverter.convert(chapter);
 
@@ -83,12 +93,26 @@ public class ChapterParser extends Source {
                 Console.error(render("<== 第 {} 次重试失败: 【{}】 原因: {}", "red"), attempt, chapter.getTitle(), e.getMessage());
                 if (attempt == config.getMaxRetryAttempts()) {
                     // 最终失败时记录日志
-                    saveDownloadErrorLog(chapter, sr, e.getMessage());
+                    saveDownloadErrorLog(chapter, e.getMessage());
                 }
             }
         }
 
         return null;
+    }
+
+    private void saveDownloadErrorLog(Chapter chapter, String errMsg) {
+        Book book = BookContext.get();
+        String line = StrUtil.format("下载失败章节: 【{}】({})\t原因: {}", chapter.getTitle(), chapter.getUrl(), errMsg);
+        String path = StrUtil.format("{}{}《{}》({}) 下载失败章节.log",
+                config.getDownloadPath(), File.separator, book.getBookName(), book.getAuthor());
+
+        try (PrintWriter pw = new PrintWriter(new FileWriter(path, StandardCharsets.UTF_8, true))) {
+            pw.println(line);
+
+        } catch (IOException e) {
+            Console.error(e);
+        }
     }
 
     /**
@@ -98,7 +122,7 @@ public class ChapterParser extends Source {
      * @param interval 爬取间隔
      */
     @SneakyThrows
-    private String crawl(String url, long interval) {
+    private String fetchContent(String url, long interval) {
         Rule.Chapter ruleChapter = this.rule.getChapter();
         Document document;
         Thread.sleep(interval);
@@ -154,18 +178,6 @@ public class ChapterParser extends Source {
         }
 
         return contentBuilder.toString();
-    }
-
-    private void saveDownloadErrorLog(Chapter chapter, SearchResult sr, String errMsg) {
-        String line = StrUtil.format("下载失败章节：【{}】({}) 原因：{}", chapter.getTitle(), chapter.getUrl(), errMsg);
-        String path = StrUtil.format("{}{}《{}》({})下载失败章节.log", config.getDownloadPath(), File.separator, sr.getBookName(), sr.getAuthor());
-
-        try (PrintWriter pw = new PrintWriter(new FileWriter(path, StandardCharsets.UTF_8, true))) {
-            pw.println(line);
-
-        } catch (IOException e) {
-            Console.error(e);
-        }
     }
 
 }
