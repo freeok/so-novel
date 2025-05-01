2a26586904619fa98bc3b7eddcceb10ab0d9b3c0
freeok <51998152+freeok@users.noreply.github.com>
2025-05-02 01:25:42 +0800

    :bug: 修复JsCaller.java线程不安全

--- src/main/java/com/pcdd/sonovel/util/JsCaller.java
+++ src/main/java/com/pcdd/sonovel/util/JsCaller.java
@@ -7,21 +7,28 @@ import org.graalvm.polyglot.Value;
 @UtilityClass
 public class JsCaller {
 
-    private final Context context = Context
-            .newBuilder("js")
-            .allowAllAccess(true)
-            .build();
-
+    /**
+     * 调用 JS 代码处理 rValue，并返回结果
+     *
+     * @param jsCode JS 代码 (动态逻辑)
+     * @param rValue 输入参数 (如 HTML 字符串)
+     * @return 处理后的结果
+     */
     public String call(String jsCode, String rValue) {
-        context.eval("js", """
-                function func(r) {
-                    %s
-                    return r;
-                }
-                """.formatted(jsCode));
-        Value func = context.getBindings("js").getMember("func");
-        Value result = func.execute(rValue);
-        return result.asString();
+        try (Context context = Context.newBuilder("js")
+                .allowAllAccess(true)
+                .build()) {
+            context.eval("js", """
+                    globalThis.__callJs = function(jsCode, r) {
+                        let func = new Function('r', jsCode + '; return r;');
+                        return func(r);
+                    };
+                    """);
+
+            Value callJsFunc = context.getBindings("js").getMember("__callJs");
+            Value result = callJsFunc.execute(jsCode, rValue);
+            return result.asString();
+        }
     }
 
 }
