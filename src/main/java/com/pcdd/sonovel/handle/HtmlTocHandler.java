cf3d0285f788b3398dbc126709c47ee1d46d37b1
freeok <51998152+freeok@users.noreply.github.com>
2025-05-06 00:34:49 +0800

    :bug: 修复txt、html格式下载封面失败导致的中断

--- src/main/java/com/pcdd/sonovel/handle/HtmlTocHandler.java
+++ src/main/java/com/pcdd/sonovel/handle/HtmlTocHandler.java
@@ -3,10 +3,8 @@ package com.pcdd.sonovel.handle;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileReader;
 import cn.hutool.core.io.file.FileWriter;
-import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.ReUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.http.HttpUtil;
 import com.pcdd.sonovel.model.Book;
 import com.pcdd.sonovel.util.FileUtils;
 
@@ -40,9 +38,7 @@ public class HtmlTocHandler implements PostProcessingHandler {
         FileWriter fw = FileWriter.create(file, StandardCharsets.UTF_8);
         fw.writeLines(strings);
 
-        Console.log("<== 正在下载封面：{}", book.getCoverUrl());
-        File coverFile = HttpUtil.downloadFileFromUrl(book.getCoverUrl(), System.getProperty("user.dir") + File.separator + saveDir);
-        FileUtil.rename(coverFile, "0_封面." + FileUtil.getType(coverFile), true);
+        downloadCover(book, saveDir);
     }
 
 }
--- src/main/java/com/pcdd/sonovel/handle/PostProcessingHandler.java
+++ src/main/java/com/pcdd/sonovel/handle/PostProcessingHandler.java
@@ -1,8 +1,13 @@
 package com.pcdd.sonovel.handle;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Console;
+import cn.hutool.http.HttpUtil;
 import com.pcdd.sonovel.model.Book;
 
 import java.io.File;
+
+import static org.fusesource.jansi.AnsiRenderer.render;
 
 /**
  * @author pcdd
@@ -12,5 +17,17 @@ public interface PostProcessingHandler {
 
     void handle(Book book, File saveDir);
 
-}
+    /**
+     * 下载封面失败会导致生成中断，必须捕获异常
+     */
+    default void downloadCover(Book book, File saveDir) {
+        try {
+            Console.log("<== 正在下载封面：{}", book.getCoverUrl());
+            File coverFile = HttpUtil.downloadFileFromUrl(book.getCoverUrl(), System.getProperty("user.dir") + File.separator + saveDir);
+            FileUtil.rename(coverFile, "0_封面." + FileUtil.getType(coverFile), true);
+        } catch (Exception e) {
+            Console.error(render("封面下载失败：{}", "red"), e.getMessage());
+        }
+    }
 
+}
