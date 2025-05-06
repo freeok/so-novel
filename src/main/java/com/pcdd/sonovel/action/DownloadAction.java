373dd752dbf0895ec6b84695867ca03fbba208c9
freeok <51998152+freeok@users.noreply.github.com>
2025-05-07 01:55:06 +0800

    :loud_sound: 修改日志样式

--- src/main/java/com/pcdd/sonovel/action/DownloadAction.java
+++ src/main/java/com/pcdd/sonovel/action/DownloadAction.java
@@ -101,7 +101,7 @@ public class DownloadAction {
         }
 
         double res = new Crawler(config).crawl(sr.getUrl(), toc);
-        Console.log("<== 完成！总耗时 {} s", NumberUtil.round(res, 2));
+        Console.log(render("<== 完成！总耗时 {} s", "green"), NumberUtil.round(res, 2));
     }
 
 }
