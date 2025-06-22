d1bb9a365fe4cb7e3ffc0910cf3f4a962f757d28
freeok <51998152+freeok@users.noreply.github.com>
2025-06-23 05:00:52 +0800

    :sparkles: 开放自定义书源功能
    官方书源：rules/official.json

--- src/main/java/com/pcdd/sonovel/core/Source.java
+++ src/main/java/com/pcdd/sonovel/core/Source.java
@@ -1,15 +1,10 @@
 package com.pcdd.sonovel.core;
 
-import cn.hutool.core.io.resource.ResourceUtil;
-import cn.hutool.core.lang.Console;
 import cn.hutool.core.lang.Opt;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSONUtil;
 import com.pcdd.sonovel.model.AppConfig;
 import com.pcdd.sonovel.model.Rule;
 import com.pcdd.sonovel.util.ConfigUtils;
-
-import static org.jline.jansi.AnsiRenderer.render;
+import com.pcdd.sonovel.util.SourceUtils;
 
 /**
  * @author pcdd
@@ -22,66 +17,18 @@ public class Source {
 
     // 配置文件读取书源 id
     public Source(AppConfig config) {
-        this(config.getSourceId(), config);
+        this(SourceUtils.getRule(config.getSourceId()), config);
     }
 
-    public Source(int id) {
-        this(id, null);
+    public Source(int sourceId) {
+        this(SourceUtils.getRule(sourceId), null);
     }
 
     // 自定义书源 id，用于测试
-    public Source(int id, AppConfig config) {
-        String jsonStr = null;
-
-        try {
-            // 根据 sourceId 获取对应书源规则
-            jsonStr = ResourceUtil.readUtf8Str("rule/rule-" + id + ".json");
-        } catch (Exception e) {
-            Console.error(render("书源规则初始化失败，请检查配置项 source-id 是否正确。{}", "red"), e.getMessage());
-            System.exit(1);
-        }
-
-        // 将 JSON 转换为 Rule 对象，并应用默认值
-        this.rule = applyDefaultRule(JSONUtil.toBean(jsonStr, Rule.class));
+    public Source(Rule rule, AppConfig config) {
+        this.rule = rule;
         // 使用配置文件中的配置，若为空则使用默认配置
         this.config = Opt.ofNullable(config).orElse(ConfigUtils.config());
-    }
-
-    private Rule applyDefaultRule(Rule rule) {
-        Rule.Search ruleSearch = rule.getSearch();
-        Rule.Book ruleBook = rule.getBook();
-        Rule.Toc ruleToc = rule.getToc();
-        Rule.Chapter ruleChapter = rule.getChapter();
-
-        // baseUri
-        if (ruleSearch != null && StrUtil.isEmpty(ruleSearch.getBaseUri())) {
-            ruleSearch.setBaseUri(rule.getUrl());
-        }
-        if (ruleBook != null && StrUtil.isEmpty(ruleBook.getBaseUri())) {
-            ruleBook.setBaseUri(rule.getUrl());
-        }
-        if (ruleToc != null && StrUtil.isEmpty(ruleToc.getBaseUri())) {
-            ruleToc.setBaseUri(rule.getUrl());
-        }
-        if (ruleChapter != null && StrUtil.isEmpty(ruleChapter.getBaseUri())) {
-            ruleChapter.setBaseUri(rule.getUrl());
-        }
-
-        // timeout
-        if (ruleSearch != null && ruleSearch.getTimeout() == null) {
-            ruleSearch.setTimeout(15);
-        }
-        if (ruleBook != null && ruleBook.getTimeout() == null) {
-            ruleBook.setTimeout(15);
-        }
-        if (ruleToc != null && ruleToc.getTimeout() == null) {
-            ruleToc.setTimeout(30);
-        }
-        if (ruleChapter != null && ruleChapter.getTimeout() == null) {
-            ruleChapter.setTimeout(10);
-        }
-
-        return rule;
     }
 
 }
