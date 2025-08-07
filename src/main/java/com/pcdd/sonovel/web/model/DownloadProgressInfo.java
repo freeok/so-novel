b0c37c6c15124f23d3c8eaaf4b01a53d4c93dbda
freeok <51998152+freeok@users.noreply.github.com>
2025-08-07 23:59:09 +0800

    :recycle: 优化ws代码

--- src/main/java/com/pcdd/sonovel/web/model/DownloadProgressInfo.java
+++ src/main/java/com/pcdd/sonovel/web/model/DownloadProgressInfo.java
@@ -1,11 +1,13 @@
 package com.pcdd.sonovel.web.model;
 
 import lombok.Builder;
+import lombok.Data;
 
 /**
  * @author pcdd
  * Created at 2025/8/7
  */
+@Data
 @Builder
 public class DownloadProgressInfo {
 
