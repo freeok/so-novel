f5a0ac2c3a7c43337a8d39cb3a82899fefa9a3ce
freeok <51998152+freeok@users.noreply.github.com>
2025-08-17 02:29:40 +0800

    :children_crossing: 改进webui下载体验

--- src/main/java/com/pcdd/sonovel/web/servlet/BookDownloadServlet.java
+++ src/main/java/com/pcdd/sonovel/web/servlet/BookDownloadServlet.java
@@ -1,39 +1,57 @@
 package com.pcdd.sonovel.web.servlet;
 
-import cn.hutool.core.lang.Console;
-import com.pcdd.sonovel.core.Crawler;
-import com.pcdd.sonovel.model.AppConfig;
-import com.pcdd.sonovel.model.SearchResult;
-import com.pcdd.sonovel.util.ConfigUtils;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import com.pcdd.sonovel.util.ConfigWatcher;
 import com.pcdd.sonovel.web.util.RespUtils;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * 从服务器下载文件到客户端
+ */
 public class BookDownloadServlet extends HttpServlet {
+
+    private static final int BUFFER_SIZE = 1024;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            SearchResult sr = SearchResult.builder()
-                    .bookName(req.getParameter("bookName"))
-                    .sourceId(Integer.parseInt(req.getParameter("sourceId")))
-                    .url(req.getParameter("url"))
-                    .author(req.getParameter("author"))
-                    .build();
+        String filename = req.getParameter("filename");
 
-            downloadFile(sr);
-            RespUtils.writeJson(resp, "开始下载");
-        } catch (Exception e) {
-            RespUtils.writeError(resp, 500, "下载失败: " + e.getMessage());
+        if (StrUtil.isBlank(filename)) {
+            RespUtils.writeError(resp, 400, "参数文件名不可为空");
+            return;
         }
+
+        downloadFileToLocal(resp, filename);
     }
 
-    private void downloadFile(SearchResult sr) {
-        AppConfig config = ConfigUtils.defaultConfig();
-        config.setSourceId(sr.getSourceId());
-        Console.log("<== 正在获取章节目录...");
-        new Crawler(config).crawl(sr.getUrl());
+    @SneakyThrows
+    private void downloadFileToLocal(HttpServletResponse resp, String filename) {
+        File file = new File(ConfigWatcher.getConfig().getDownloadPath(), filename);
+
+        if (!file.exists()) {
+            RespUtils.writeError(resp, 404, "文件不存在");
+            return;
+        }
+
+        resp.setContentType("application/octet-stream");
+        resp.setHeader("Content-Disposition", "attachment;filename=" + URLUtil.encode(filename));
+
+        try (FileInputStream in = new FileInputStream(file);
+             ServletOutputStream out = resp.getOutputStream()) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int len;
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+        }
     }
 
 }
---
+++ src/main/java/com/pcdd/sonovel/web/servlet/BookFetchServlet.java
@@ -0,0 +1,37 @@
+package com.pcdd.sonovel.web.servlet;
+
+import cn.hutool.core.lang.Console;
+import com.pcdd.sonovel.core.Crawler;
+import com.pcdd.sonovel.model.AppConfig;
+import com.pcdd.sonovel.model.SearchResult;
+import com.pcdd.sonovel.util.ConfigUtils;
+import com.pcdd.sonovel.util.SourceUtils;
+import com.pcdd.sonovel.web.util.RespUtils;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class BookFetchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            String bookUrl = req.getParameter("url");
+            SearchResult sr = SearchResult.builder()
+                    .url(bookUrl)
+                    .sourceId(SourceUtils.getSource(bookUrl).getId())
+                    .build();
+            downloadFileToServer(sr);
+        } catch (Exception e) {
+            RespUtils.writeError(resp, 500, "下载失败: " + e.getMessage());
+        }
+    }
+
+    private void downloadFileToServer(SearchResult sr) {
+        AppConfig config = ConfigUtils.defaultConfig();
+        config.setSourceId(sr.getSourceId());
+        Console.log("<== 正在获取章节目录...");
+        new Crawler(config).crawl(sr.getUrl());
+    }
+
+}
