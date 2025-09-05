ac4563fc51ee9a5ca90ee2e027c40b415d528e3c
freeok <51998152+freeok@users.noreply.github.com>
2025-09-06 01:22:16 +0800

    :zap: 移除fonts目录，从系统字体获取，优化体积

--- bin/release-macos.sh
+++ bin/release-macos.sh
@@ -46,7 +46,6 @@ mkdir -p "target/$dist_dirname"
 
 cp "bundle/$jre_filename" "target/$dist_dirname"
 cp -r bundle/rules "target/$dist_dirname"
-cp -r bundle/fonts "target/$dist_dirname"
 cp bundle/config.ini bundle/readme.txt bundle/run-macos.sh "target/$dist_dirname"
 
 # 复制 jar
