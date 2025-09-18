ba295dac1699c2f426208ed49a84d8591361c67a
freeok <51998152+freeok@users.noreply.github.com>
2025-09-18 23:35:04 +0800

    :hammer: 优化 Linux, macOS 启动脚本

--- bundle/run-linux.sh
+++ bundle/run-linux.sh
@@ -1,1 +1,18 @@
-chmod 777 ./runtime/bin/java && ./runtime/bin/java -Dconfig.file=config.ini -jar app.jar
+#!/bin/bash
+
+# 设置权限
+chmod 777 ./runtime/bin/java || { echo "设置权限失败"; exit 1; }
+
+# 检查文件是否存在
+[ -f ./runtime/bin/java ] || { echo "未找到 Java 可执行文件"; exit 1; }
+[ -f app.jar ] || { echo "未找到 app.jar"; exit 1; }
+[ -f config.ini ] || { echo "未找到 config.ini"; exit 1; }
+
+# 运行 Java 应用
+./runtime/bin/java \
+  -XX:+UseZGC \
+  -XX:+ZGenerational \
+  -DjrePath=runtime \
+  -Dconfig.file=config.ini \
+  -Dmode=tui \
+  -jar app.jar || { echo "运行 Java 应用失败"; exit 1; }
