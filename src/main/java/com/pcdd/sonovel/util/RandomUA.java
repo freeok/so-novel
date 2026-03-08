3b09c294a45e1b22deb829845894467862119053
freeok <51998152+freeok@users.noreply.github.com>
2026-03-08 14:11:16 +0800

    :recycle: 更新随机 UA

--- src/main/java/com/pcdd/sonovel/util/RandomUA.java
+++ src/main/java/com/pcdd/sonovel/util/RandomUA.java
@@ -11,24 +11,23 @@ import java.util.Random;
 @UtilityClass
 public class RandomUA {
 
-    private final String[] OPERATING_SYSTEMS = {
+    private final String[] DESKTOP_OS = {
+            "Windows NT 6.1; Win64; x64",
             "Windows NT 10.0; Win64; x64",
             "Windows NT 11.0; Win64; x64",
-            "Macintosh; Intel Mac OS X 12_6",
-            "Macintosh; Intel Mac OS X 13_4",
+            "Macintosh; Intel Mac OS X 10_15_7",
             "X11; Linux x86_64",
-            "X11; Ubuntu; Linux x86_64"
+            "X11; Ubuntu; Linux x86_64",
     };
 
     private final String[] BROWSERS = {"Chrome", "Firefox", "Safari", "Edge"};
-
-    private final int MIN_VERSION = 100; // Minimum browser version
-    private final int MAX_VERSION = 131; // Maximum browser version
+    private final int MIN_VERSION = 86; // Minimum browser version
+    private final int MAX_VERSION = 145; // Maximum browser version
 
     private final Random RANDOM = new Random();
 
     public String generate() {
-        String operatingSystem = OPERATING_SYSTEMS[RANDOM.nextInt(OPERATING_SYSTEMS.length)];
+        String operatingSystem = DESKTOP_OS[RANDOM.nextInt(DESKTOP_OS.length)];
         String browser = BROWSERS[RANDOM.nextInt(BROWSERS.length)];
         int majorVersion = RANDOM.nextInt(MAX_VERSION - MIN_VERSION + 1) + MIN_VERSION;
         int minorVersion = RANDOM.nextInt(10); // Minor version is 0-9
@@ -41,7 +40,7 @@ public class RandomUA {
             case "Firefox" -> String.format("Mozilla/5.0 (%s; rv:%d.0) Gecko/20100101 %s/%d.0",
                     operatingSystem, majorVersion, browser, majorVersion);
             case "Safari" ->
-                    String.format("Mozilla/5.0 (%s) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/%d.1 Safari/605.1.15",
+                    String.format("Mozilla/5.0 (%s) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/%d.1 Safari/537.36",
                             operatingSystem, majorVersion);
             case "Edge" ->
                     String.format("Mozilla/5.0 (%s) AppleWebKit/537.36 (KHTML, like Gecko) %s/%d.0.%d.0 Safari/537.36",
