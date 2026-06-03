bfb5fda1d6ea04ad7f30a761640e08ce2e5db0e0
freeok <51998152+freeok@users.noreply.github.com>
2026-06-03 16:58:28 +0800

    :bookmark: release v1.10.2

--- src/main/java/com/pcdd/sonovel/action/DonateAction.java
+++ src/main/java/com/pcdd/sonovel/action/DonateAction.java
@@ -12,8 +12,8 @@ public class DonateAction {
 
     public static void execute() {
         Console.log(render("感谢您的支持！请通过以下方式赞助：\n", "red", "bold"));
-        Console.log(render("支付宝： https://ghfast.top/github.com/freeok/so-novel/blob/main/assets/donation-alipay.png?raw=true", "blue", "bold"));
-        Console.log(render("微信：   https://ghfast.top/github.com/freeok/so-novel/blob/main/assets/donation-wechat.jpg?raw=true", "green", "bold"));
+        Console.log(render("支付宝： https://gh-proxy.org/github.com/freeok/so-novel/blob/main/assets/donation-alipay.png", "blue", "bold"));
+        Console.log(render("微信：   https://gh-proxy.org/github.com/freeok/so-novel/blob/main/assets/donation-wechat.jpg", "green", "bold"));
         Console.log(render("(打开链接图片后，扫码即可赞助，您的支持是我持续更新的动力！)", "ITALIC"));
     }
 
