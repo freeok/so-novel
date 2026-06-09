bc08ce40b8c5826f0b41f7c5725dd128aed0a9c3
freeok <51998152+freeok@users.noreply.github.com>
2026-06-10 02:28:55 +0800

    :art: 规范 alpinejs 代码

---
+++ src/main/resources/static/js/api.js
@@ -0,0 +1,20 @@
+const Api = {
+
+  getConfig() {
+    return fetch('/config').then(r => r.json())
+  },
+
+  getLocalBooks() {
+    return fetch('/local-books').then(r => r.json())
+  },
+
+  search(keyword) {
+    return fetch(`/search/aggregated?kw=${encodeURIComponent(keyword)}`)
+      .then(r => r.json())
+  },
+
+  downloadBook(params) {
+    return fetch(`/book-fetch?${params.toString()}`)
+  },
+
+}
