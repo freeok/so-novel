caea837c2db5b086acff8d52025aa9d8d3248e3e
freeok <51998152+freeok@users.noreply.github.com>
2026-07-07 20:01:18 +0800

    :recycle: 优化 BookParser.java

--- src/main/java/com/pcdd/sonovel/parser/BookParser.java
+++ src/main/java/com/pcdd/sonovel/parser/BookParser.java
@@ -5,13 +5,13 @@ import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
 import com.pcdd.sonovel.context.HttpClientContext;
-import com.pcdd.sonovel.utils.CoverUpdater;
 import com.pcdd.sonovel.core.HtmlExtractor;
 import com.pcdd.sonovel.core.Source;
 import com.pcdd.sonovel.model.AppConfig;
 import com.pcdd.sonovel.model.ContentType;
 import com.pcdd.sonovel.model.Rule.Book;
 import com.pcdd.sonovel.utils.ChineseConverter;
+import com.pcdd.sonovel.utils.CoverUpdater;
 import com.pcdd.sonovel.utils.CrawlUtils;
 import lombok.SneakyThrows;
 import okhttp3.OkHttpClient;
@@ -36,53 +36,50 @@ public class BookParser extends Source {
     @SneakyThrows
     public Book parse(String url) {
         Book r = this.rule.getBook();
+        Document document = fetchDocument(url, r.getTimeout(), r.getBaseUri());
+        document = handleCloudflareBypass(document, url);
 
-        Document document;
-        try (Response resp = CrawlUtils.request(httpClient, url, r.getTimeout());
+        String bookName = HtmlExtractor.extract(document, r.getBookName());
+        String author = HtmlExtractor.extract(document, r.getAuthor());
+        Assert.isTrue(StrUtil.isAllNotEmpty(bookName, author), "详情页书名或作者不能为空！DOM:\n{}\n", document);
+        String defaultCoverUrl = HtmlExtractor.extract(document, r.getCoverUrl(), getContentType(r.getCoverUrl()));
+
+        Book book = new Book();
+        book.setUrl(url);
+        book.setBookName(bookName);
+        book.setAuthor(author.replace("作者：", ""));
+        book.setIntro(StrUtil.cleanBlank(HtmlExtractor.extract(document, r.getIntro())));
+        // 代理 IP 会被起点等网站屏蔽，故使用源站封面
+        book.setCoverUrl(this.rule.isNeedProxy() ? defaultCoverUrl : CoverUpdater.fetchCover(book, defaultCoverUrl));
+        book.setCategory(HtmlExtractor.extract(document, r.getCategory()));
+        book.setLatestChapter(HtmlExtractor.extract(document, r.getLatestChapter()));
+        book.setLatestChapterUrl(HtmlExtractor.extract(document, r.getLatestChapterUrl()));
+        book.setLastUpdateTime(HtmlExtractor.extract(document, r.getLastUpdateTime()).replaceAll("(更新时间|最后更新)：", ""));
+        book.setStatus(HtmlExtractor.extract(document, r.getStatus()));
+
+        return ChineseConverter.convert(book, this.rule.getLanguage(), config.getLanguage());
+    }
+
+    @SneakyThrows
+    private Document fetchDocument(String url, int timeout, String baseUri) {
+        try (Response resp = CrawlUtils.request(httpClient, url, timeout);
              InputStream is = resp.body().byteStream()) {
-            // null 表示自动检测编码
-            document = Jsoup.parse(is, null, r.getBaseUri());
+            return Jsoup.parse(is, null, baseUri);
         }
+    }
 
+    private Document handleCloudflareBypass(Document document, String url) {
         if (CrawlUtils.hasCf(document)) {
             Assert.isTrue(StrUtil.isNotEmpty(config.getCfBypass()), "🤖 检测到详情页 {} 存在 Cloudflare 真人验证，但未设置 cf-bypass 配置项，故跳过", url);
             Console.log("🤖 检测到详情页 {} 存在 Cloudflare 真人验证，正在尝试绕过...", url);
             String realHtml = HttpUtil.get("%s/html?url=%s".formatted(this.config.getCfBypass(), url));
             document = Jsoup.parse(realHtml);
         }
-
-        String bookName = HtmlExtractor.extract(document, r.getBookName(), getContentType(r.getBookName()));
-        String author = HtmlExtractor.extract(document, r.getAuthor(), getContentType(r.getAuthor()));
-        Assert.isTrue(StrUtil.isAllNotEmpty(bookName, author), "详情页书名或作者不能为空！DOM:\n{}\n", document);
-        // 以下为非必须属性
-        String intro = StrUtil.cleanBlank(HtmlExtractor.extract(document, r.getIntro(), getContentType(r.getIntro())));
-        String defaultCoverUrl = HtmlExtractor.extract(document, r.getCoverUrl(), getContentType(r.getCoverUrl()));
-        String category = HtmlExtractor.extract(document, r.getCategory(), getContentType(r.getCategory()));
-        String latestChapter = HtmlExtractor.extract(document, r.getLatestChapter(), getContentType(r.getLatestChapter()));
-        String latestChapterUrl = HtmlExtractor.extract(document, r.getLatestChapterUrl(), getContentType(r.getLatestChapterUrl()));
-        String lastUpdateTime = HtmlExtractor.extract(document, r.getLastUpdateTime(), getContentType(r.getLastUpdateTime()));
-        String status = HtmlExtractor.extract(document, r.getStatus(), getContentType(r.getStatus()));
-
-        Book book = new Book();
-        book.setUrl(url);
-        book.setBookName(bookName);
-        book.setAuthor(author.replace("作者：", ""));
-        book.setIntro(intro);
-        // 代理 IP 会被起点等网站屏蔽，故使用源站封面
-        book.setCoverUrl(this.rule.isNeedProxy() ? defaultCoverUrl : CoverUpdater.fetchCover(book, defaultCoverUrl));
-        book.setCategory(category);
-        book.setLatestChapter(latestChapter);
-        book.setLatestChapterUrl(latestChapterUrl);
-        book.setLastUpdateTime(lastUpdateTime.replaceAll("(更新时间|最后更新)：", ""));
-        book.setStatus(status);
-
-        return ChineseConverter.convert(book, this.rule.getLanguage(), config.getLanguage());
+        return document;
     }
 
     /**
      * 部分书源的 meta 格式不标准，需自定义 meta 规则，故仍需要判断 ContentType
-     *
-     * @param query Selector or XPath
      */
     private ContentType getContentType(String query) {
         if (StrUtil.isBlank(query)) return null;
